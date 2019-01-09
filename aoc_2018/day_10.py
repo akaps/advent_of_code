e9681604878f9711d66a1a469e8679c1d47f6e04
Ari <ari.kaps@gmail.com>
2019-01-08 22:48:01 -0500

    More refactoring
    storing bounds as instance variables, making get_bounds called by update

--- aoc_2018/day_10.py
+++ aoc_2018/day_10.py
@@ -37,6 +37,7 @@ class Star:
 class Constellations:
     def __init__(self, input):
         self.parse_stars(input)
+        self.get_bounds()
 
     def parse_stars(self, input):
         self.stars = []
@@ -49,24 +50,20 @@ class Constellations:
                 (int)(line[4])))
 
     def find_letters(self):
-        x_range, y_range = self.get_bounds()
         prev_size = math.inf
-        while x_range * y_range < prev_size:
-            prev_size = x_range * y_range
+        while self.x_range * self.y_range < prev_size:
+            prev_size = self.x_range * self.y_range
             print(prev_size)
             self.update(1)
-            x_range, y_range = self.get_bounds()
         self.update(-1)
         self.show()
 
     def get_bounds(self):
-        x_min = self.x_min()
-        x_max = max(self.stars, key=lambda a: a.coord.x).coord.x
-        y_min = self.y_min()
-        y_max = max(self.stars, key=lambda a: a.coord.y).coord.y
-        x_range = x_max - x_min + 1
-        y_range = y_max - y_min + 1
-        return x_range, y_range
+        xs = (self.x_min(), max(self.stars, key=lambda a: a.coord.x).coord.x)
+        ys = (self.y_min(), max(self.stars, key=lambda a: a.coord.y).coord.y)
+        self.bounds = Point(xs, ys)
+        self.x_range = self.bounds.x[1] - self.bounds.x[0]
+        self.y_range = self.bounds.y[1] - self.bounds.y[0]
 
     def x_min(self):
         return min(self.stars, key=lambda a: a.coord.x).coord.x
@@ -76,10 +73,9 @@ class Constellations:
 
     def show(self):
         # PIL accesses images in Cartesian co-ordinates, so it is Image[columns, rows]
-        x_range, y_range = self.get_bounds()
         x_min = self.x_min()
         y_min = self.y_min()
-        img = Image.new('RGB', (x_range, y_range), 'black') # create a new black image
+        img = Image.new('RGB', (self.x_range + 1, self.y_range + 1), 'black') # create a new black image
         pixels = img.load() # create the pixel map
         for star in self.stars:
             pixels[star.coord.x + x_min, star.coord.y + y_min] = (254, 0, 100) # set the colour accordingly
@@ -88,6 +84,7 @@ class Constellations:
     def update(self, incr):
         for star in self.stars:
             star.update(incr)
+        self.get_bounds()
 
 file = open('day_10_sample.txt')
 input = file.readlines()

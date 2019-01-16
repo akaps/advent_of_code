193ab13a0ff84c35c29b006d37d9fe3b5d4c1e72
Ari <ari.kaps@gmail.com>
2019-01-16 14:28:51 -0800

    reporting max of day 11 part 2? need to store x, y positions

--- aoc_2018/day_11/day_11.py
+++ aoc_2018/day_11/day_11.py
@@ -5,22 +5,22 @@ class Cells:
     def __init__(self, serial, size):
         self.serial = serial
         self.cells = []
+        self.cell_sums = []
         for _ in range(size):
             self.cells.append([None] * size)
-        for x in range(1, size + 1):
-            for y in range(1, size + 1):
-                x_t, y_t = true_coord(x, y)
-                self.cells[x_t][y_t] = calculate(x, y, self.serial)
+            self.cell_sums.append([None] * size)
+        for x in range(size):
+            for y in range(0, size):
+                self.cells[x][y] = calculate(x + 1, y + 1, self.serial)
 
     def find_most_power(self):
-        x_max, y_max = 0, 0
+        x_max, y_max = -1, -1
         max_power = -math.inf
-        for x in range(1, len(self.cells) - 2):
-            for y in range(1, len(self.cells) - 2):
-                x_t, y_t = true_coord(x, y)
-                total = self.total_power(x_t, y_t)
+        for x in range(len(self.cells) - 2):
+            for y in range(len(self.cells) - 2):
+                total = self.total_power(x, y)
                 if total > max_power:
-                    x_max, y_max = x, y
+                    x_max, y_max = x + 1, y + 1
                     max_power = total
         return x_max, y_max
 
@@ -32,13 +32,28 @@ class Cells:
         return total
 
     def most_power_square(self):
+        self.summed_areas()
+        max_ending = max_so_far = self.cell_sums[0][0]
+        for x in range(1, len(self.cell_sums)):
+            for y in range(1, len(self.cell_sums[x])):
+                max_ending = max(self.cell_sums[x][y], self.cell_sums[x][y] + max_ending)
+                max_so_far = max(max_so_far, max_ending)
+        print(max_so_far)
         return -1, -1, -1
 
-def calculate(x, y, serial):
-    return ((y * (x+10) + serial) * (x+10) // 100 % 10) - 5
+    def summed_areas(self):
+        for x in range(len(self.cells)):
+            for y in range(len(self.cells[x])):
+                self.cell_sums[x][y] = self.summed_area(x, y)
 
-def true_coord(x, y):
-    return x-1, y-1
+    def summed_area(self, x, y):
+        return (self.cells[x][y] +
+                self.cell_sums[x][y - 1] if y - 1 >= 0 else 0 +
+                self.cell_sums[x - 1][y] if x - 1 >= 0 else 0 -
+                self.cell_sums[x - 1][y - 1] if x - 1 >=0 and y - 1 >= 0 else 0)
+
+def calculate(x, y, serial):
+    return ((y * (x + 10) + serial) * (x + 10) // 100 % 10) - 5
 
 assert calculate(3, 5, 8) == 4
 sample = Cells(8, 5)

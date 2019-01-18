d12e358ccb6b0fae481f959e540dce8b8decc263
Ari <ari.kaps@gmail.com>
2019-01-17 18:36:10 -0800

    quick solution to day 1 part 2

--- aoc_2017/day_1.py
+++ aoc_2017/day_1.py
@@ -1,7 +1,9 @@
 class InverseCaptcha:
     def __init__(self, input):
         self.digits = [0] * 10
+        self.half_digits = [0] * 10
         self.reverse_captcha(input)
+        self.reverse_half(input)
 
     def reverse_captcha(self, input):
         length = len(input)
@@ -9,14 +11,22 @@ class InverseCaptcha:
             if input[i] == input[(i + 1) % length]:
                 self.digits[(int)(input[i])] += 1
 
-    def total(self):
-        print(self.digits)
-        sum = 0
-        for i in range(len(self.digits)):
-            sum += i * self.digits[i]
-        return sum
+    def reverse_half(self, input):
+        length = len(input)
+        half = length // 2
+        for i in range(length):
+            if input[i] == input[(i + half) % length]:
+                self.half_digits[(int)(input[i])] += 1
+
+def total(digits):
+    print(digits)
+    sum = 0
+    for i in range(len(digits)):
+        sum += i * digits[i]
+    return sum
 
 file = open('day_1_input.txt')
 captcha = InverseCaptcha(file.readline().strip())
 file.close()
-print(captcha.total())
+print(total(captcha.digits))
+print(total(captcha.half_digits))

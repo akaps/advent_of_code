1be39da6a08aeb5a4a80708e294c92237ef8de8b
Ari <ari.kaps@gmail.com>
2019-01-17 18:44:01 -0800

    consolidated solution to day 1 part 2

--- aoc_2017/day_1.py
+++ aoc_2017/day_1.py
@@ -1,22 +1,10 @@
-class InverseCaptcha:
-    def __init__(self, input):
-        self.digits = [0] * 10
-        self.half_digits = [0] * 10
-        self.reverse_captcha(input)
-        self.reverse_half(input)
-
-    def reverse_captcha(self, input):
-        length = len(input)
-        for i in range(length):
-            if input[i] == input[(i + 1) % length]:
-                self.digits[(int)(input[i])] += 1
-
-    def reverse_half(self, input):
-        length = len(input)
-        half = length // 2
-        for i in range(length):
-            if input[i] == input[(i + half) % length]:
-                self.half_digits[(int)(input[i])] += 1
+def reverse_captcha(input, distance):
+    digits = [0] * 10
+    length = len(input)
+    for i in range(length):
+        if input[i] == input[(i + distance) % length]:
+            digits[(int)(input[i])] += 1
+    return digits
 
 def total(digits):
     print(digits)
@@ -25,8 +13,14 @@ def total(digits):
         sum += i * digits[i]
     return sum
 
+def solve(input, distance):
+    digits = reverse_captcha(input, distance)
+    tot = total(digits)
+    print('Total for distance {dist} is {total}'.format(dist=distance, total=tot))
+
 file = open('day_1_input.txt')
-captcha = InverseCaptcha(file.readline().strip())
+input = file.readline().strip()
 file.close()
-print(total(captcha.digits))
-print(total(captcha.half_digits))
+
+solve(input, 1)
+solve(input, len(input) // 2)

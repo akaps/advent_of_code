bcb029936ad90e11f03d1298ace96cc41ed1f19b
Ari <ari.kaps@gmail.com>
2019-02-08 12:55:34 -0800

    Add implentation of reading from input
    still incorrect answer, 155 is too low for input

--- aoc_2018/day_16/day_16.py
+++ aoc_2018/day_16/day_16.py
@@ -1,4 +1,4 @@
-from operator import methodcaller
+import re
 
 def is_op(op_name, opcode, reg_bef, reg_aft):
     check = op_name(opcode, reg_bef)
@@ -111,10 +111,43 @@ op_names = [addr,
 def find_triples(ops):
     total = 0
     for op in ops:
-        if possibilities == 3:
+        if possibilities(op['opcode'], op['before'], op['after']) == 3:
             total += 1
+    return total
+
+def parse_before(line):
+    return [int(d) for d in re.findall('\d', line)]
+
+def parse_opcode(line):
+    return [int(d) for d in re.split(' ', line)]
+
+def parse_after(line):
+    return [int(d) for d in re.findall('\d', line)]
 
 sample_code = [9, 2, 1, 2]
 sample_reg_bef = [3, 2, 1, 1]
 sample_reg_aft = [3, 2, 2, 1]
 assert 3 == possibilities(sample_code, sample_reg_bef, sample_reg_aft)
+assert sample_code == parse_opcode('9 2 1 2')
+assert sample_reg_bef == parse_before('Before: [3, 2, 1, 1]')
+assert sample_reg_aft == parse_after('After:  [3, 2, 2, 1]')
+
+ops = []
+with open('day_16_input_a.txt', 'r') as file:
+    while True:
+        op = {}
+        #before line
+        op['before'] = parse_before(file.readline())
+        #opcode line
+        op['opcode'] = parse_opcode(file.readline())
+        #after line
+        op['after'] = parse_after(file.readline())
+        ops.append(op)
+        empty_line = file.readline()
+        if not empty_line:
+            break
+print(ops[0])
+assert len(ops) == 809
+ans = find_triples(ops)
+assert ans < 809
+print('Answer to part 1: {ans}'.format(ans=ans))

4ada73cae2454ce8be25f6c6ee254c527d4ca6ed
Ari <ari.kaps@gmail.com>
2019-02-08 03:05:21 -0800

    dynamic programming solution to part 1, first attempt at part 2
    both methods mutate the recipes instance now. Adding validation for
    part 2 samples without running part 1. This is a check of part 2 logic
    since the answer will be much larger than part 1's recipe

--- aoc_2018/day_14/day_14.py
+++ aoc_2018/day_14/day_14.py
@@ -6,16 +6,15 @@ class Recipes:
         self.recipe = [3, 7]
         self.current = 0
         self.next = 1
+        self.processed = 0
 
-    def run(self, total):
-        i = 0
-        while i < total + 10:
-            score = self.recipe[self.current] + self.recipe[self.next]
-            score = [int(d) for d in str(score)]
-            self.recipe.extend(score)
-            self.current = (self.current + (self.recipe[self.current] + 1)) % len(self.recipe)
-            self.next = (self.next + (self.recipe[self.next] + 1)) % len(self.recipe)
-            i += len(score)
+    def step(self):
+        score = self.recipe[self.current] + self.recipe[self.next]
+        score = [int(d) for d in str(score)]
+        self.recipe.extend(score)
+        self.current = (self.current + (self.recipe[self.current] + 1)) % len(self.recipe)
+        self.next = (self.next + (self.recipe[self.next] + 1)) % len(self.recipe)
+        return len(score)
 
     def __repr__(self):
         pretty = copy.deepcopy(self.recipe)
@@ -27,19 +26,35 @@ class Recipes:
         return ' '.join(map(str, pretty))
 
     def next_ten(self, num):
+        while self.processed < num + 10:
+            self.processed += self.step()
         return ''.join(map(str, self.recipe[num:num+10]))
 
-#easy sample
+    def length_before(self, seq, step_size):
+        output = ''.join(map(str, self.recipe))
+        while seq not in output:
+            for _ in range(step_size):
+                self.processed += self.step()
+            output = ''.join(map(str, self.recipe))
+        return output.index(seq)
+
+#sample
 recipes = Recipes()
-#print(recipes)
-recipes.run(9)
 assert recipes.next_ten(9) == '5158916779'
+assert recipes.length_before('5158916779', 1) == 9
+empty = Recipes()
+assert empty.length_before('5158916779', 1) == 9
 assert recipes.next_ten(5) == '0124515891'
-#big sample
-recipes.run(2018)
+assert recipes.length_before('0124515891', 1) == 5
 assert recipes.next_ten(18) == '9251071085'
+assert recipes.length_before('9251071085', 1) == 18
 assert recipes.next_ten(2018) == '5941429882'
+assert recipes.length_before('5941429882', 1) == 2018
+
 #input
-print('part 1')
-recipes.run(409551)
-print(recipes.next_ten(409551))
+ans = recipes.next_ten(409551)
+assert ans == '1631191756'
+print('Answer to part 1: {ans}'.format(ans=ans))
+
+print('Answer to part 2:')
+print(recipes.length_before('409551', 100000000))

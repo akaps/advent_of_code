53c93fc8a4ecefdc9545b5d0aee74e0d025c46b6
Ari <ari.kaps@gmail.com>
2019-02-08 02:05:01 -0800

    solution to day 14 part 1, with validation

---
+++ aoc_2018/day_14/day_14.py
@@ -0,0 +1,51 @@
+import copy
+
+class Recipes:
+
+    def __init__(self):
+        self.recipe = [3, 7]
+        self.current = 0
+        self.next = 1
+
+    def run(self, total):
+        i = 0
+        while i < total + 10:
+            score = self.recipe[self.current] + self.recipe[self.next]
+            score = [int(d) for d in str(score)]
+            self.recipe.extend(score)
+            self.current = (self.current + (self.recipe[self.current] + 1)) % len(self.recipe)
+            self.next = (self.next + (self.recipe[self.next] + 1)) % len(self.recipe)
+            i += len(score)
+
+    def __repr__(self):
+        pretty = copy.deepcopy(self.recipe)
+        for i in range(len(pretty)):
+            if i == self.current:
+                pretty[i] = '({num})'.format(num=pretty[i])
+            elif i == self.next:
+                pretty[i] = '[{num}]'.format(num=pretty[i])
+        return ' '.join(map(str, pretty))
+
+    def next_ten(self, num):
+        return ''.join(map(str, self.recipe[num:num+10]))
+
+#easy sample
+recipes = Recipes()
+#print(recipes)
+recipes.run(9)
+print('easy example')
+print(recipes.next_ten(9))
+assert recipes.next_ten(9) == '5158916779'
+print(recipes.next_ten(5))
+assert recipes.next_ten(5) == '0124515891'
+#big sample
+print('big example')
+recipes.run(2018)
+print(recipes.next_ten(18))
+assert recipes.next_ten(18) == '9251071085'
+print(recipes.next_ten(2018))
+assert recipes.next_ten(2018) == '5941429882'
+#input
+print('answer')
+recipes.run(409551)
+print(recipes.next_ten(409551))

8484cd50e4317520576d3a606d4b0aff72bb303c
Ari <ari.kaps@gmail.com>
2019-02-08 13:53:45 -0800

    Adding more tests to determine where the bug is
    all opcodes under tests
    parsing under tests
    confirming sample is passing for the correct opcodes
    still no answer...

--- aoc_2018/day_16/day_16.py
+++ aoc_2018/day_16/day_16.py
@@ -124,14 +124,45 @@ def parse_opcode(line):
 def parse_after(line):
     return [int(d) for d in re.findall('\d', line)]
 
+#sample input
 sample_code = [9, 2, 1, 2]
 sample_reg_bef = [3, 2, 1, 1]
 sample_reg_aft = [3, 2, 2, 1]
 assert 3 == possibilities(sample_code, sample_reg_bef, sample_reg_aft)
+assert is_op(mulr, sample_code, sample_reg_bef, sample_reg_aft)
+assert is_op(addi, sample_code, sample_reg_bef, sample_reg_aft)
+assert is_op(seti, sample_code, sample_reg_bef, sample_reg_aft)
+
+#parsing tests
 assert sample_code == parse_opcode('9 2 1 2')
 assert sample_reg_bef == parse_before('Before: [3, 2, 1, 1]')
 assert sample_reg_aft == parse_after('After:  [3, 2, 2, 1]')
 
+#arithmetic ops tests
+assert is_op(addr, [-1, 0, 1, 2], [1, 3, 0, -1], [1, 3, 4, -1])
+assert is_op(addi, [-1, 0, 8, 2], [3, -1, -1, -1], [3, -1, 11, -1])
+assert is_op(mulr, [-1, 0, 1, 2], [3, 2, -1, -1], [3, 2, 6, -1])
+assert is_op(muli, [-1, 3, 18, 0], [-1, -1, -1, 2], [36, -1, -1, 2])
+assert is_op(banr, [-1, 2, 1, 0], [-1, 13, 60, -1], [12, 13, 60, -1])
+assert is_op(bani, [-1, 1, 13, 0], [-1, 60, -1, -1], [12, 60, -1, -1])
+assert is_op(borr, [-1, 2, 1, 0], [-1, 13, 60, -1], [61, 13, 60, -1])
+assert is_op(bori, [-1, 1, 13, 0], [-1, 60, -1, -1], [61, 60, -1, -1])
+assert is_op(setr, [-1, 3, -92, 2], [-1, -1, -1, 96], [-1, -1, 96, 96])
+assert is_op(seti, [-1, 200, 10000, 3], [-1, -1, -1, -1], [-1, -1, -1, 200])
+
+#comparison ops tests
+assert is_op(gtir, [-1, 19, 0, 3], [-1, -1, -1, -1], [-1, -1, -1, 1])
+assert is_op(gtir, [-1, -19, 0, 3], [-1, -1, -1, -1], [-1, -1, -1, 0])
+assert is_op(gtri, [-1, 2, 2, 0], [-1, -3, 3, -1], [1, -3, 3, -1])
+assert is_op(gtri, [-1, 1, 2, 0], [-1, -3, 3, -1], [0, -3, 3, -1])
+assert is_op(gtrr, [-1, 1, 2, 3], [-1, 1, 2, -1], [-1, 1, 2, 0])
+assert is_op(gtrr, [-1, 1, 2, 3], [-1, 2, 1, -1], [-1, 2, 1, 1])
+assert is_op(eqri, [-1, 0, 19, 3], [19, -1, -1, -1], [19, -1, -1, 1])
+assert is_op(eqri, [-1, 0, 19, 3], [20, -1, -1, -1], [20, -1, -1, 0])
+assert is_op(eqrr, [-1, 1, 2, 3], [-1, 1, 1, -1], [-1, 1, 1, 1])
+assert is_op(eqrr, [-1, 1, 2, 3], [-1, 420, 69, -1], [-1, 420, 69, 0])
+
+#real input
 ops = []
 with open('day_16_input_a.txt', 'r') as file:
     while True:
@@ -146,7 +177,6 @@ with open('day_16_input_a.txt', 'r') as file:
         empty_line = file.readline()
         if not empty_line:
             break
-print(ops[0])
 assert len(ops) == 809
 ans = find_triples(ops)
 assert ans < 809

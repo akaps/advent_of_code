1e611363f78b4874790336f97eb8d1cd1c870397
Ari <ari.kaps@gmail.com>
2019-02-09 14:19:00 -0800

    dictionary solution, with mistake
    D is getting the wrong start value

--- aoc_2018/day07/dependencies.py
+++ aoc_2018/day07/dependencies.py
@@ -39,65 +39,86 @@ class Dependencies:
                 dependencies[step].remove(to_remove)
 
 
-    def order_in_parallel(self, time_bonus):
-        processing = [[None, 0], [None, 0]]
+    def order_in_parallel(self, num_processors, time_bonus):
+        processing = {}
         order = ''
         to_process = copy.deepcopy(self.dependencies)
         time = 0
-        print('Second\tWorker 1\tWorker 2\tDone')
-        while to_process or (processing[0][0] or processing[1][0]):
+        pretty_print_header(num_processors)
+        while to_process or processing:
             steps = list(to_process.keys())
             steps.sort()
-            if not processing[0][0] or not processing[1][1]:
-                for step in steps:
-                    if not to_process[step]:
-                        if not processing[0][0]:
-                            processing[0] = [step, time_for_step(step, time_bonus)]
-                            to_process.pop(step)
-                        elif not processing[1][0]:
-                            processing[1] = [step, time_for_step(step, time_bonus)]
-                            to_process.pop(step)
-            if processing[0][0]:
-                processing[0][1] -= 1
-            if processing[1][0]:
-                processing[1][1] -= 1
-            print('{sec}\t{t1}\t\t{t2}\t\t{done}'.format(
-                sec=time,
-                t1=processing[0][0] if processing[0][0] else '-',
-                t2=processing[1][0] if processing[1][0] else '-',
-                done=order))
-            if processing[0][0] and processing[0][1] <= 0:
-                to_remove = processing[0][0]
-                order += to_remove
-                processing[0][0] = None
-                for step in to_process:
-                    if to_remove in to_process[step]:
-                        to_process[step].remove(to_remove)
-            if processing[1][0] and processing[1][1] <= 0:
-                to_remove = processing[1][0]
-                order += to_remove
-                processing[1][0] = None
-                for step in to_process:
-                    if to_remove in to_process[step]:
-                        to_process[step].remove(to_remove)
-            time += 1
+            populate_processors(processing, num_processors, steps, to_process, time_bonus)
+            pretty_print_line(time, processing, num_processors, order)
+            time += update_time(processing)
+            order = complete_processors(processing, to_process, order)
         return order, time
+
+def complete_processors(processing, to_process, order):
+    for key in list(processing):
+        if processing[key] <= 0:
+            processing.pop(key)
+            order += key
+            for step in to_process:
+                if key in to_process[step]:
+                    to_process[step].remove(key)
+    return order
+
+def update_time(processing):
+    smallest = min(processing, key=processing.get)
+    time = processing[smallest]
+    print('smallest time {t} for {item}'.format(t=time, item=smallest))
+    time_update = processing[min(processing, key=processing.get)]
+    for item in processing.items():
+        processing.update({item[0]: item[1] - time_update})
+    return time_update
+
+def populate_processors(processing, num_processors, steps, to_process, time_bonus):
+    if len(processing) < num_processors:
+        for step in steps:
+            if not to_process[step]:
+                processing[step] = time_for_step(step, time_bonus)
+                to_process.pop(step)
+
+def pretty_print_header(num_processors):
+    result = ['Second']
+    for i in range(num_processors):
+        result.append('Worker {i}'.format(i=i))
+    result.append('Done')
+    print('\t'.join(result))
+
+def pretty_print_line(time, processing, num_processors, order):
+    result = [str(time)]
+    keys = list(processing)
+    for i in range(num_processors):
+        if i < len(keys):
+            result.append(keys[i])
+        else:
+            result.append('-')
+    result.append(order)
+    print('\t\t'.join(result))
 
 def time_for_step(step, time_bonus):
     return ord(step) - ord('A') + 1 + time_bonus
 
-def solve_for(file_name, step_size):
+def solve_for(file_name, processors, step_size):
     dep = Dependencies(file_name)
     order = dep.order()
-    order_parallel, time_parallel = dep.order_in_parallel(step_size)
+    order_parallel, time_parallel = dep.order_in_parallel(processors, step_size)
 
     return order, order_parallel, time_parallel
 
-ORDER, ORDER_PARALLEL, TIME = solve_for('sample.txt', 0)
+assert time_for_step('A', 0) == 1
+assert time_for_step('B', 0) == 2
+assert time_for_step('C', 0) == 3
+assert time_for_step('D', 0) == 4
+assert time_for_step('E', 0) == 5
+assert time_for_step('F', 0) == 6
+ORDER, ORDER_PARALLEL, TIME = solve_for('sample.txt', 2, 0)
 assert ORDER == 'CABDFE'
-assert ORDER_PARALLEL == 'CABFDE'
 assert TIME == 15
+assert ORDER_PARALLEL == 'CABFDE'
 
-# ORDER, ORDER_PARALLEL, TIME = solve_for('input.txt', 60)
-# print('single-threaded sequence is {order}'.format(order=ORDER))
-# print('multi-threaded time was {time}'.format(time=TIME))
+ORDER, ORDER_PARALLEL, TIME = solve_for('input.txt', 5, 60)
+print('single-threaded sequence is {order}'.format(order=ORDER))
+print('multi-threaded time was {time}'.format(time=TIME))

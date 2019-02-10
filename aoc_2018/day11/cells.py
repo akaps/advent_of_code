d0f16587fc1d83b50d17cdd7db8957b769720f35
Ari <ari.kaps@gmail.com>
2019-02-09 17:08:47 -0800

    refactor all the things

--- aoc_2018/day11/cells.py
+++ aoc_2018/day11/cells.py
@@ -3,72 +3,87 @@ import math
 #reminder: top-left is 1, 1. x, y notation
 class Cells:
     def __init__(self, serial, size):
-        self.serial = serial
+        self.populate_cells(size, serial)
+
+    def populate_cells(self, size, serial):
         self.cells = []
         self.cell_sums = []
         for _ in range(size):
-            self.cells.append([None] * size)
-            self.cell_sums.append([None] * size)
-        for x in range(size):
-            for y in range(0, size):
-                self.cells[x][y] = calculate(x + 1, y + 1, self.serial)
+            self.cells.append([0] * size)
+            self.cell_sums.append([0] * size)
+        for row in range(size):
+            for col in range(0, size):
+                self.cells[row][col] = calculate(row + 1, col + 1, serial)
+                self.cell_sums[row][col] = self.summed_area(row, col)
 
     def find_most_power(self):
         x_max, y_max = -1, -1
         max_power = -math.inf
-        for x in range(len(self.cells) - 2):
-            for y in range(len(self.cells) - 2):
-                total = self.total_power(x, y)
+        for row in range(len(self.cells) - 2):
+            for col in range(len(self.cells) - 2):
+                total = self.total_power(row, col)
                 if total > max_power:
-                    x_max, y_max = x + 1, y + 1
+                    x_max, y_max = row + 1, col + 1
                     max_power = total
         return x_max, y_max
 
-    def total_power(self, x, y):
+    def total_power(self, row, col):
         total = 0
         for i in range(3):
             for j in range(3):
-                total += self.cells[x + i][y + j]
+                total += self.cells[row + i][col + j]
         return total
 
+    #2D implementation of Kadane's algorithm
     def most_power_square(self):
-        self.summed_areas()
-        max_ending = max_so_far = self.cell_sums[0][0]
-        for x in range(1, len(self.cell_sums)):
-            for y in range(1, len(self.cell_sums[x])):
-                max_ending = max(self.cell_sums[x][y], self.cell_sums[x][y] + max_ending)
-                max_so_far = max(max_so_far, max_ending)
-        print(max_so_far)
-        return -1, -1, -1
+        max_sum = -999999999999
+        max_x = max_y = max_size = -1
+        for size in range(1, 300):
+            for row in range(300-size):
+                for col in range(300-size):
+                    if self.sum_area(row, col, size) > max_sum:
+                        max_x = row
+                        max_y = col
+                        max_size = size
+        return max_x, max_y, max_size
 
-    def summed_areas(self):
-        for x in range(len(self.cells)):
-            for y in range(len(self.cells[x])):
-                self.cell_sums[x][y] = self.summed_area(x, y)
+    def sum_area(self, row, col, size):
+        return (self.cell_sums[row][col] -
+                self.cell_sums[row - size][col] -
+                self.cell_sums[row][col - size] +
+                self.cell_sums[row - size][col - size])
 
-    def summed_area(self, x, y):
-        return (self.cells[x][y] +
-                self.cell_sums[x][y - 1] if y - 1 >= 0 else 0 +
-                self.cell_sums[x - 1][y] if x - 1 >= 0 else 0 -
-                self.cell_sums[x - 1][y - 1] if x - 1 >=0 and y - 1 >= 0 else 0)
+    def summed_area(self, row, col):
+        prev_col = col - 1
+        prev_row = row - 1
+        bot_right = self.cells[row][col]
+        bot_left = self.cell_sums[row][prev_col] if col - 1 >= 0 else 0
+        top_right = self.cell_sums[prev_row][col] if row - 1 >= 0 else 0
+        top_left = self.cell_sums[prev_row][prev_col] if row - 1 >= 0 and col - 1 >= 0 else 0
+        return bot_right + bot_left + top_right - top_left
 
-    def power_square(self, x, y, size):
+    def power_square(self, row, col, size):
         total = 0
-        x -= 1
-        y -= 1
-        for i in range(x, x+size):
-            for j in range(y, y+size):
+        row -= 1
+        col -= 1
+        for i in range(row, row+size):
+            for j in range(col, col+size):
                 total += self.cells[i][j]
         return total
 
-
-def calculate(x, y, serial):
-    return ((y * (x + 10) + serial) * (x + 10) // 100 % 10) - 5
+def calculate(row, col, serial):
+    return ((col * (row + 10) + serial) * (row + 10) // 100 % 10) - 5
 
 assert calculate(3, 5, 8) == 4
 assert calculate(122, 79, 57) == -5
 assert calculate(217, 196, 39) == 0
 assert calculate(101, 153, 71) == 4
+
+GRID__SIZE_3 = Cells(18, 2)
+assert GRID__SIZE_3.cell_sums[0][0] == -2
+assert GRID__SIZE_3.cell_sums[0][1] == -3
+assert GRID__SIZE_3.cell_sums[1][0] == -4
+assert GRID__SIZE_3.cell_sums[1][1] == -5
 
 GRID_18 = Cells(18, 300)
 X, Y = GRID_18.find_most_power()

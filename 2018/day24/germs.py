95778b1ca9a538d429e3a83646aac38c5de82966
Ari <ari.kaps@gmail.com>
2019-02-16 17:02:47 -0800

    Adding structure for handling combat and asserts for output

--- 2018/day24/germs.py
+++ 2018/day24/germs.py
@@ -10,6 +10,29 @@ class Germs:
         self.armies['Immune System'] = [Group(line) for line in text[1:infection_group - 1]]
         self.armies['Infection'] = [Group(line) for line in text[infection_group + 1:]]
 
+    def combat(self):
+        while len(self.armies) > 1:
+            selections = self.select_targets()
+            self.fight_targets(selections)
+
+    def select_targets(self):
+        imm_targets = []
+        for group in self.armies['Immune System']:
+            imm_targets.append(group.select_target(self.armies['Infection']))
+        germ_targets = []
+        for group in self.armies['Infection']:
+            germ_targets.append(group.select_target(self.armies['Immune System']))
+        return imm_targets, germ_targets
+
+    def fight_targets(self, selections):
+        pass
+
+    def remaining_units(self, name):
+        total = 0
+        for group in self.armies[name]:
+            total += group.num_units
+        return total
+
 class Group:
     def __init__(self, input_text):
         vals = re.findall(r'\d+', input_text)
@@ -28,9 +51,9 @@ class Group:
         return self.num_units * self.attack_damage
 
     def select_target(self, other_groups):
-        pass
+        return None
 
-    def take_damaage(self, amount):
+    def take_damage(self, amount):
         self.num_units -= amount // self.hit_points
 
     def element_modifier(self, damage_type):
@@ -41,4 +64,12 @@ class Group:
         return 1
 
 SAMPLE = Germs('sample.txt')
+SAMPLE.combat()
+assert SAMPLE.remaining_units('Immune System') == 0
+assert SAMPLE.remaining_units('Infection') == 5216
+
 INPUT = Germs('input.txt')
+INPUT.combat()
+print('results of Part 1')
+print('Immune System: {power}'.format(power=INPUT.remaining_units('Immune System')))
+print('Infection: {power}'.format(power=INPUT.remaining_units('Infection')))

1a62b60ffe33b0f8c4b7ad896b51af43f08e7fb7
Ari <ari.kaps@gmail.com>
2019-02-24 12:25:58 -0800

    Dynamic programming solution for day 5
    Don't reevaluate the first 8 hashes, track the last number evaluated

--- 2016/day05/hashing.py
+++ 2016/day05/hashing.py
@@ -1,40 +1,50 @@
-import utils
 import hashlib
-import itertools
+import utils
 
-def password(door_id):
-    res = ''
-    for i in itertools.count():
-        hash_string = door_id + str(i)
-        hashed = hashlib.md5(hash_string.encode()).hexdigest()
-        if hashed[0:5] == '00000':
-            print(door_id, i, hashed)
-            res += hashed[5]
-        if len(res) == 8:
-            break
-    print(res)
-    return res
+class Password:
+    def __init__(self, door_id):
+        self.door_id = door_id
+        self.valid_hashes = []
+        self.i = 0
 
-def password2(door_id):
-    res = [0, 0, 0, 0, 0, 0, 0, 0]
-    for i in itertools.count():
-        hash_string = door_id + str(i)
-        hashed = hashlib.md5(hash_string.encode()).hexdigest()
-        if (
-                hashed[0:5] == '00000' and
-                hashed[5].isdigit() and
-                int(hashed[5]) < 8 and
-                res[int(hashed[5])] == 0
-            ):
-            print(door_id, i, hashed)
-            res[int(hashed[5])] = hashed[6]
-        if 0 not in res:
-            break
-    print(''.join(res))
-    return ''.join(res)
+    def find_first_password(self):
+        res = ''
+        for _ in range(8):
+            next_hash = self.determine_next_hash()
+            self.valid_hashes.append(next_hash)
+            res += next_hash[0]
+        return res
 
-assert password('abc') == '18f47a30'
-utils.pretty_print_answer(1, password('uqwqemis'))
+    def find_second_password(self):
+        res = [0, 0, 0, 0, 0, 0, 0, 0]
+        for hash_val in self.valid_hashes:
+            index = hash_val[0]
+            if valid_index(index) and not res[int(index)]:
+                res[int(index)] = hash_val[1]
+        while 0 in res:
+            next_hash = self.determine_next_hash()
+            index = next_hash[0]
+            if valid_index(index) and not res[int(index)]:
+                res[int(index)] = next_hash[1]
+        return ''.join(res)
 
-assert password2('abc') == '05ace8e3'
-utils.pretty_print_answer(2, password2('uqwqemis'))
+    def determine_next_hash(self):
+        hashed = 'xxxxx'
+        while hashed[0:5] != '00000':
+            hash_string = self.door_id + str(self.i)
+            hashed = hashlib.md5(hash_string.encode()).hexdigest()
+            self.i += 1
+        self.valid_hashes.append(hashed[5:])
+        return hashed[5:]
+
+def valid_index(index):
+    return index.isdigit() and int(index) < 8
+
+SAMPLE = Password('abc')
+assert SAMPLE.find_first_password() == '18f47a30'
+
+PROBLEM = Password('uqwqemis')
+utils.pretty_print_answer(1, PROBLEM.find_first_password())
+
+assert SAMPLE.find_second_password() == '05ace8e3'
+utils.pretty_print_answer(2, PROBLEM.find_second_password())

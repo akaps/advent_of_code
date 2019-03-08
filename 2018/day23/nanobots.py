21d61d96cb03872086740ce4a99f79692a898b56
Ari <ari.kaps@gmail.com>
2019-03-07 17:09:28 -0800

    Refactor nanobots to get range of multiple bots
    this calculation is needed for part 2. Generally cleaning up the code

--- 2018/day23/nanobots.py
+++ 2018/day23/nanobots.py
@@ -1,32 +1,39 @@
 import re
-import utils as utils
+import utils
+
+class Nanobot:
+    def __init__(self, loc, radius):
+        self.position = loc
+        self.radius = radius
 
 class Nanobots:
-    def __init__(self, lines):
-        self.radius = -1
-        self.center = None
+    def __init__(self, file_name):
+        file = open(file_name, 'r')
+        lines = file.readlines()
+        file.close()
         self.nanobots = []
         for line in lines:
             x, y, z, rad = map(int, re.findall('-?\d+', line))
-            if rad > self.radius:
-                self.radius = rad
-                self.center = (x, y, z)
-            self.nanobots.append((x, y, z))
+            self.nanobots.append(Nanobot((x, y, z), rad))
 
-    def within_cluster(self):
+    def bot_with_largest_radius(self):
+        bot_with_largest_radius = self.nanobots[0]
+        for bot in self.nanobots:
+            if bot.radius > bot_with_largest_radius.radius:
+                bot_with_largest_radius = bot
+        return bot_with_largest_radius
+
+    def nanobots_in_range(self, nanobot):
         total = 0
-        for nanobot in self.nanobots:
-            dist = utils.manhattan_distance(nanobot, self.center)
-            if dist <= self.radius:
+        for bot in self.nanobots:
+            if utils.manhattan_distance(bot.position, nanobot.position) <= nanobot.radius:
                 total += 1
         return total
 
-def determine_cluster(file_name):
-    file = open(file_name, 'r')
-    nanobots = Nanobots(file.readlines())
-    file.close()
-    return nanobots.within_cluster()
+def solve_part_1(bots):
+    return bots.nanobots_in_range(bots.bot_with_largest_radius())
 
-assert determine_cluster('sample.txt') == 7
-ANSWER = determine_cluster('input.txt')
-print('Answer to part 1: {ans}'.format(ans=ANSWER))
+SAMPLE = Nanobots('sample.txt')
+assert solve_part_1(SAMPLE) == 7
+PROBLEM = Nanobots('input.txt')
+print('Answer to part 1: {ans}'.format(ans=solve_part_1(PROBLEM)))

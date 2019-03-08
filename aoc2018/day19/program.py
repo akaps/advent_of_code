116162a892786b1c7e24187671915267a7a7d2f1
Ari <ari.kaps@gmail.com>
2019-03-07 22:30:42 -0800

    solution to day19 part 1

--- aoc2018/day19/program.py
+++ aoc2018/day19/program.py
@@ -1,29 +1,39 @@
 import re
-from computer import Computer
+import utils
+import aoc2018.computer as comp
 
-class Registers:
-    def __init__(self, ip, instr):
+class Program:
+    def __init__(self, file_name):
+        file = open(file_name)
+        lines = file.readlines()
+        file.close()
         self.registers = [0] * 6
-        self.registers[0] = ip
-        self.instructions = instr
+        self.ip_index = determine_ip(lines.pop(0))
+        self.instructions = parse_lines(lines)
 
-    def do_instruction(self, instruction):
-        operation = re.split(' ', instruction)
-        for i in range(1, 6):
-            operation[i] = int(operation[i])
-        operation[0](operation[1:])
+    def run(self):
+        while 0 <= self.registers[self.ip_index] < len(self.instructions):
+            ip = self.registers[self.ip_index]
+            opcode, a, b, c = self.instructions[ip]
+            output = comp.execute(opcode, self.registers, a, b, c)
+            self.registers = output
+            self.registers[self.ip_index] += 1
 
-def process_input(file_name):
-    file = open(file_name, 'r')
-    ip = int(re.findall('\d', file.readline())[0])
-    instructions = file.readlines()
-    file.close()
-    computer = Computer(6, ip, instructions)
-    computer.run()
-    return computer.registers[0]
+def determine_ip(line):
+    return int(re.search('\d', line).group(0))
 
-answer = process_input('sample.txt')
-assert answer == 7
+def parse_lines(lines):
+    res = []
+    for line in lines:
+        parsed = re.split((r' '), line.strip())
+        instruction = (parsed[0], int(parsed[1]), int(parsed[2]), int(parsed[3]))
+        res.append(instruction)
+    return res
 
-answer = process_input('input.txt')
-print('Part 1 answer: {ans}'.format(ans=answer))
+SAMPLE = Program('sample.txt')
+SAMPLE.run()
+assert SAMPLE.registers[0] == 7
+
+PROBLEM = Program('input.txt')
+PROBLEM.run()
+utils.pretty_print_answer(1, PROBLEM.registers[0])

0420eabfa10983a4435ff3c3b2e3207b4efff596
Ari <ari.kaps@gmail.com>
2019-03-07 20:41:59 -0800

    Cleaning up and solving day16
    using lambdas instead of static methods in computer.py
    the problem was looking for instructions that match 3 or more
    Had correct solution either way

--- aoc2018/computer.py
+++ aoc2018/computer.py
@@ -1,11 +1,27 @@
+INSTRUCTIONS = {'addr' : lambda regs, a, b: regs[a] + regs[b],
+                'addi' : lambda regs, a, b: regs[a] + b,
+                'mulr' : lambda regs, a, b: regs[a] * regs[b],
+                'muli' : lambda regs, a, b: regs[a] * b,
+                'banr' : lambda regs, a, b: regs[a] & regs[b],
+                'bani' : lambda regs, a, b: regs[a] & b,
+                'borr' : lambda regs, a, b: regs[a] | regs[b],
+                'bori' : lambda regs, a, b: regs[a] | b,
+                'setr' : lambda regs, a, b: regs[a],
+                'seti' : lambda regs, a, b: a,
+                'gtir' : lambda regs, a, b: 1 if a > regs[b] else 0,
+                'gtri' : lambda regs, a, b: 1 if regs[a] > b else 0,
+                'gtrr' : lambda regs, a, b: 1 if regs[a] > regs[b] else 0,
+                'eqir' : lambda regs, a, b: 1 if a == regs[b] else 0,
+                'eqri' : lambda regs, a, b: 1 if regs[a] == b else 0,
+                'eqrr' : lambda regs, a, b: 1 if regs[a] == regs[b] else 0}
+
 class Computer:
-    def __init__(self, num_registers, start_ip, instructions):
+    def __init__(self, num_registers, start_ip, program):
         self.registers = [0] * num_registers
         self.ip = start_ip
-        self.process_instructions(instructions)
+        self.process_program(program)
 
-    def process_instructions(self, instructions):
-        self.instructions=[]
+    def process_program(self, instructions):
         for instruction in instructions:
             instruction = instruction.split()
             for i in range(1, 4):
@@ -21,106 +37,4 @@ class Computer:
         command = self.instructions[ip]
         cmd = getattr(self, command[0])
         res = cmd(command, self.registers)
-        # print('ip={ip} {reg} {cmd} {after}'.format(
-        #     ip=ip,
-        #     reg=self.registers,
-        #     cmd=command,
-        #     after=res
-        # ))
         self.registers = res
-
-    @staticmethod
-    def addr(command, reg):
-        res = list(reg)
-        res[command[3]] = res[command[1]] + res[command[2]]
-        return res
-
-    @staticmethod
-    def addi(command, reg):
-        res = list(reg)
-        res[command[3]] = res[command[1]] + command[2]
-        return res
-
-    @staticmethod
-    def mulr(command, reg):
-        res = list(reg)
-        res[command[3]] = res[command[1]] * res[command[2]]
-        return res
-
-    @staticmethod
-    def muli(command, reg):
-        res = list(reg)
-        res[command[3]] = res[command[1]] * command[2]
-        return res
-
-    @staticmethod
-    def banr(command, reg):
-        res = list(reg)
-        res[command[3]] = res[command[1]] & res[command[2]]
-        return res
-
-    @staticmethod
-    def bani(command, reg):
-        res = list(reg)
-        res[command[3]] = res[command[1]] & command[2]
-        return res
-
-    @staticmethod
-    def borr(command, reg):
-        res = list(reg)
-        res[command[3]] = res[command[1]] | res[command[2]]
-        return res
-
-    @staticmethod
-    def bori(command, reg):
-        res = list(reg)
-        res[command[3]] = res[command[1]] | command[2]
-        return res
-
-    @staticmethod
-    def setr(command, reg):
-        res = list(reg)
-        res[command[3]] = res[command[1]]
-        return res
-
-    @staticmethod
-    def seti(command, reg):
-        res = list(reg)
-        res[command[3]] = command[1]
-        return res
-
-    @staticmethod
-    def gtir(command, reg):
-        res = list(reg)
-        res[command[3]] = 1 if command[1] > res[command[2]] else 0
-        return res
-
-    @staticmethod
-    def gtri(command, reg):
-        res = list(reg)
-        res[command[3]] = 1 if res[command[1]] > command[2] else 0
-        return res
-
-    @staticmethod
-    def gtrr(command, reg):
-        res = list(reg)
-        res[command[3]] = 1 if res[command[1]] > res[command[2]] else 0
-        return res
-
-    @staticmethod
-    def eqir(command, reg):
-        res = list(reg)
-        res[command[3]] = 1 if command[1] == res[command[2]] else 0
-        return res
-
-    @staticmethod
-    def eqri(command, reg):
-        res = list(reg)
-        res[command[3]] = 1 if res[command[1]] == command[2] else 0
-        return res
-
-    @staticmethod
-    def eqrr(command, reg):
-        res = list(reg)
-        res[command[3]] = 1 if res[command[1]] == res[command[2]] else 0
-        return res
--- aoc2018/day16/registers.py
+++ aoc2018/day16/registers.py
@@ -1,38 +1,25 @@
 import re
-from aoc2018.computer import Computer
+from aoc2018.computer import INSTRUCTIONS
 
-def is_command(command_name, command, reg_bef, reg_aft):
-    check = command_name(command, reg_bef)
-    return check == reg_aft
+def is_command(cmd_name, code, reg, result):
+    print(cmd_name, code, reg, result)
+    in_a, in_b = code[1], code[2]
+    out = INSTRUCTIONS[cmd_name](reg, in_a, in_b)
+    output = list(result)
+    output[code[3]] = out
+    return output == result
 
-def possibilities(command, reg_bef, reg_aft):
-    matches = 0
-    for command_name in command_names:
-        if is_command(command_name, command, reg_bef, reg_aft):
-            matches += 1
-    return matches
-
-command_names = [Computer.addr,
-                 Computer.addi,
-                 Computer.mulr,
-                 Computer.muli,
-                 Computer.banr,
-                 Computer.bani,
-                 Computer.borr,
-                 Computer.bori,
-                 Computer.setr,
-                 Computer.seti,
-                 Computer.gtir,
-                 Computer.gtri,
-                 Computer.gtrr,
-                 Computer.eqir,
-                 Computer.eqri,
-                 Computer.eqrr]
+def possibilities(code, reg, result):
+    total = 0
+    for cmd_name in INSTRUCTIONS:
+        if is_command(cmd_name, code, reg, result):
+            total += 1
+    return total
 
 def find_triples(ops):
     total = 0
     for op in ops:
-        if possibilities(op['command'], op['before'], op['after']) == 3:
+        if possibilities(op['command'], op['before'], op['after']) >= 3:
             total += 1
     return total
 
@@ -46,42 +33,39 @@ def parse_after(line):
     return [int(d) for d in re.findall('\d', line)]
 
 #sample input
-sample_code = [9, 2, 1, 2]
-sample_reg_bef = [3, 2, 1, 1]
-sample_reg_aft = [3, 2, 2, 1]
-assert 3 == possibilities(sample_code, sample_reg_bef, sample_reg_aft)
-assert is_command(Computer.mulr, sample_code, sample_reg_bef, sample_reg_aft)
-assert is_command(Computer.addi, sample_code, sample_reg_bef, sample_reg_aft)
-assert is_command(Computer.seti, sample_code, sample_reg_bef, sample_reg_aft)
+assert is_command('mulr', [9, 2, 1, 2], [3, 2, 1, 1], [3, 2, 2, 1])
+assert is_command('addi', [9, 2, 1, 2], [3, 2, 1, 1], [3, 2, 2, 1])
+assert is_command('seti', [9, 2, 1, 2], [3, 2, 1, 1], [3, 2, 2, 1])
+assert 3 == possibilities([9, 2, 1, 2], [3, 2, 1, 1], [3, 2, 2, 1])
 
 #parsing tests
-assert sample_code == parse_command('9 2 1 2')
-assert sample_reg_bef == parse_before('Before: [3, 2, 1, 1]')
-assert sample_reg_aft == parse_after('After:  [3, 2, 2, 1]')
+assert [9, 2, 1, 2] == parse_command('9 2 1 2')
+assert [3, 2, 1, 1] == parse_before('Before: [3, 2, 1, 1]')
+assert [3, 2, 2, 1] == parse_after('After:  [3, 2, 2, 1]')
 
 #arithmetic ops tests
-assert is_command(Computer.addr, [-1, 0, 1, 2], [1, 3, 0, -1], [1, 3, 4, -1])
-assert is_command(Computer.addi, [-1, 0, 8, 2], [3, -1, -1, -1], [3, -1, 11, -1])
-assert is_command(Computer.mulr, [-1, 0, 1, 2], [3, 2, -1, -1], [3, 2, 6, -1])
-assert is_command(Computer.muli, [-1, 3, 18, 0], [-1, -1, -1, 2], [36, -1, -1, 2])
-assert is_command(Computer.banr, [-1, 2, 1, 0], [-1, 13, 60, -1], [12, 13, 60, -1])
-assert is_command(Computer.bani, [-1, 1, 13, 0], [-1, 60, -1, -1], [12, 60, -1, -1])
-assert is_command(Computer.borr, [-1, 2, 1, 0], [-1, 13, 60, -1], [61, 13, 60, -1])
-assert is_command(Computer.bori, [-1, 1, 13, 0], [-1, 60, -1, -1], [61, 60, -1, -1])
-assert is_command(Computer.setr, [-1, 3, -92, 2], [-1, -1, -1, 96], [-1, -1, 96, 96])
-assert is_command(Computer.seti, [-1, 200, 10000, 3], [-1, -1, -1, -1], [-1, -1, -1, 200])
+assert is_command('addr', [-1, 0, 1, 2], [1, 3, 0, -1], [1, 3, 4, -1])
+assert is_command('addi', [-1, 0, 8, 2], [3, -1, -1, -1], [3, -1, 11, -1])
+assert is_command('mulr', [-1, 0, 1, 2], [3, 2, -1, -1], [3, 2, 6, -1])
+assert is_command('muli', [-1, 3, 18, 0], [-1, -1, -1, 2], [36, -1, -1, 2])
+assert is_command('banr', [-1, 2, 1, 0], [-1, 13, 60, -1], [12, 13, 60, -1])
+assert is_command('bani', [-1, 1, 13, 0], [-1, 60, -1, -1], [12, 60, -1, -1])
+assert is_command('borr', [-1, 2, 1, 0], [-1, 13, 60, -1], [61, 13, 60, -1])
+assert is_command('bori', [-1, 1, 13, 0], [-1, 60, -1, -1], [61, 60, -1, -1])
+assert is_command('setr', [-1, 3, -92, 2], [-1, -1, -1, 96], [-1, -1, 96, 96])
+assert is_command('seti', [-1, 200, 10000, 3], [-1, -1, -1, -1], [-1, -1, -1, 200])
 
 #comparison ops tests
-assert is_command(Computer.gtir, [-1, 19, 0, 3], [-1, -1, -1, -1], [-1, -1, -1, 1])
-assert is_command(Computer.gtir, [-1, -19, 0, 3], [-1, -1, -1, -1], [-1, -1, -1, 0])
-assert is_command(Computer.gtri, [-1, 2, 2, 0], [-1, -3, 3, -1], [1, -3, 3, -1])
-assert is_command(Computer.gtri, [-1, 1, 2, 0], [-1, -3, 3, -1], [0, -3, 3, -1])
-assert is_command(Computer.gtrr, [-1, 1, 2, 3], [-1, 1, 2, -1], [-1, 1, 2, 0])
-assert is_command(Computer.gtrr, [-1, 1, 2, 3], [-1, 2, 1, -1], [-1, 2, 1, 1])
-assert is_command(Computer.eqri, [-1, 0, 19, 3], [19, -1, -1, -1], [19, -1, -1, 1])
-assert is_command(Computer.eqri, [-1, 0, 19, 3], [20, -1, -1, -1], [20, -1, -1, 0])
-assert is_command(Computer.eqrr, [-1, 1, 2, 3], [-1, 1, 1, -1], [-1, 1, 1, 1])
-assert is_command(Computer.eqrr, [-1, 1, 2, 3], [-1, 420, 69, -1], [-1, 420, 69, 0])
+assert is_command('gtir', [-1, 19, 0, 3], [-1, -1, -1, -1], [-1, -1, -1, 1])
+assert is_command('gtir', [-1, -19, 0, 3], [-1, -1, -1, -1], [-1, -1, -1, 0])
+assert is_command('gtri', [-1, 2, 2, 0], [-1, -3, 3, -1], [1, -3, 3, -1])
+assert is_command('gtri', [-1, 1, 2, 0], [-1, -3, 3, -1], [0, -3, 3, -1])
+assert is_command('gtrr', [-1, 1, 2, 3], [-1, 1, 2, -1], [-1, 1, 2, 0])
+assert is_command('gtrr', [-1, 1, 2, 3], [-1, 2, 1, -1], [-1, 2, 1, 1])
+assert is_command('eqri', [-1, 0, 19, 3], [19, -1, -1, -1], [19, -1, -1, 1])
+assert is_command('eqri', [-1, 0, 19, 3], [20, -1, -1, -1], [20, -1, -1, 0])
+assert is_command('eqrr', [-1, 1, 2, 3], [-1, 1, 1, -1], [-1, 1, 1, 1])
+assert is_command('eqrr', [-1, 1, 2, 3], [-1, 420, 69, -1], [-1, 420, 69, 0])
 
 #real input
 commands = []

267dbdef5af2be59b938f062dab778c630f9803d
Ari <ari.kaps@gmail.com>
2019-03-09 00:19:33 -0800

    refactoring part 1, solution to part 2
    part 2 determines power of arbitrary sized squares
    add size parameter to find_most_power
    Using numpy to quickly generate the array
    Doing black magic to learn about convolutions in python

--- aoc2018/day11/cells.py
+++ aoc2018/day11/cells.py
@@ -1,114 +1,72 @@
 import math
+import numpy
 
 #reminder: top-left is 1, 1. x, y notation
 class Cells:
     def __init__(self, serial, size):
-        self.populate_cells(size, serial)
+        self.serial = serial
+        self.cells = numpy.fromfunction(self.cell_val, (size, size), dtype=int)
 
-    def populate_cells(self, size, serial):
-        self.cells = []
-        self.cell_sums = []
-        for _ in range(size):
-            self.cells.append([0] * size)
-            self.cell_sums.append([0] * size)
-        for row in range(size):
-            for col in range(0, size):
-                self.cells[row][col] = calculate(row + 1, col + 1, serial)
-                self.cell_sums[row][col] = self.summed_area(row, col)
+    def cell_val(self, row, col):
+        return calculate_cell(row, col, self.serial)
+
+    def find_most_power_for_size(self, size):
+        sums = sum(self.cells[x:x - size + 1 or None, y:y - size + 1 or None]
+                  for x in range(size) for y in range(size))
+        maximum = sums.max()
+        indexes = numpy.where(sums == maximum)
+        max_row, max_col = indexes[0][0], indexes[1][0]
+        return max_row, max_col, maximum
 
     def find_most_power(self):
-        x_max, y_max = -1, -1
-        max_power = -math.inf
-        for row in range(len(self.cells) - 2):
-            for col in range(len(self.cells) - 2):
-                total = self.total_power(row, col)
-                if total > max_power:
-                    x_max, y_max = row + 1, col + 1
-                    max_power = total
-        return x_max, y_max
+        max_x, max_y, max_power, max_size = -1, -1, -math.inf, -1
+        for size in range(1, 50):
+            row, col, power = self.find_most_power_for_size(size)
+            if power > max_power:
+                max_x, max_y, max_power, max_size = row, col, power, size
+        return max_x, max_y, max_power, max_size
 
-    def total_power(self, row, col):
-        total = 0
-        for i in range(3):
-            for j in range(3):
-                total += self.cells[row + i][col + j]
-        return total
+    def __repr__(self):
+        result = []
+        for row in self.cells:
+            result.append(' '.join([str(x) for x in row]))
+        return '\n'.join(result)
 
-    def most_power_square(self):
-        max_sum = self.cell_sums[0][0]
-        max_x = max_y = max_size = -1
-        for size in range(1, 300):
-            for row in range(301 - size):
-                for col in range(301 - size):
-                    if self.sum_area(row, col, size) > max_sum:
-                        max_x = row
-                        max_y = col
-                        max_size = size
-        return max_x, max_y, max_size
-
-    def sum_area(self, row, col, size):
-        row -= 1
-        col -= 1
-        return (self.cell_sums[row][col]
-                - self.cell_sums[row - size][col]
-                - self.cell_sums[row][col - size]
-                + self.cell_sums[row - size][col - size])
-
-    def summed_area(self, row, col):
-        prev_col = col - 1
-        prev_row = row - 1
-        return (self.cells[row][col]
-                + (self.cell_sums[row][prev_col] if prev_col >= 0 else 0)
-                + (self.cell_sums[prev_row][col] if prev_row >= 0 else 0)
-                - (self.cell_sums[prev_row][prev_col] if prev_row >= 0 and prev_col >= 0 else 0))
-
-    def power_square(self, row, col, size):
-        total = 0
-        row -= 1
-        col -= 1
-        for i in range(row, row + size):
-            for j in range(col, col + size):
-                total += self.cells[i][j]
-        return total
-
-def calculate(row, col, serial):
+def calculate_cell(row, col, serial):
     return ((col * (row + 10) + serial) * (row + 10) // 100 % 10) - 5
 
-assert calculate(3, 5, 8) == 4
-assert calculate(122, 79, 57) == -5
-assert calculate(217, 196, 39) == 0
-assert calculate(101, 153, 71) == 4
+assert calculate_cell(3, 5, 8) == 4
+assert calculate_cell(122, 79, 57) == -5
+assert calculate_cell(217, 196, 39) == 0
+assert calculate_cell(101, 153, 71) == 4
 
 GRID_18 = Cells(18, 300)
-X, Y = GRID_18.find_most_power()
+X, Y, POWER = GRID_18.find_most_power_for_size(3)
 assert X == 33
 assert Y == 45
-assert GRID_18.power_square(33, 45, 3) == 29
-assert GRID_18.sum_area(33, 45, 3) == 29
+assert POWER == 29
 
 GRID_42 = Cells(42, 300)
-X, Y = GRID_42.find_most_power()
+X, Y, POWER = GRID_42.find_most_power_for_size(3)
 assert X == 21
 assert Y == 61
-assert GRID_42.power_square(21, 61, 3) == 30
-assert GRID_18.sum_area(21, 61, 3) == 30
-
-X, Y, SIZE = GRID_18.most_power_square()
-assert GRID_18.power_square(90, 269, 16) == 113
-assert GRID_18.sum_area(90, 269, 16) == 113
-assert X == 90
-assert Y == 269
-assert SIZE == 16
-
-X, Y, SIZE = GRID_42.most_power_square()
-assert GRID_42.power_square(232, 251, 12) == 119
-assert GRID_18.sum_area(232, 251, 12) == 119
-assert X == 232
-assert Y == 251
-assert SIZE == 12
+assert POWER == 30
 
 CELLS = Cells(7803, 300)
-X, Y = CELLS.find_most_power()
-print('most 3x3 power at ({x},{y})'.format(x=X, y=Y))
-X, Y, SIZE = CELLS.most_power_square()
-print('most power at square ({x},{y},{size})'.format(x=X, y=Y, size=SIZE))
+X, Y, POWER = CELLS.find_most_power_for_size(3)
+print('most 3x3 power is {pow} at {x},{y}'.format(pow=POWER, x=X, y=Y))
+
+X, Y, POWER, SIZE = GRID_18.find_most_power()
+assert X == 90
+assert Y == 269
+assert POWER == 113
+assert SIZE == 16
+
+X, Y, POWER, SIZE = GRID_42.find_most_power()
+assert X == 232
+assert Y == 251
+assert POWER == 119
+assert SIZE == 12
+
+X, Y, POWER, SIZE = CELLS.find_most_power()
+print('most power is {pow} at square {x},{y},{size}'.format(pow=POWER, x=X, y=Y, size=SIZE))

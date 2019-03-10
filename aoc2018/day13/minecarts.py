bbd2af2069b0def7b11027e413680dd0184414f7
Ari <ari.kaps@gmail.com>
2019-03-09 20:43:09 -0800

    refactoring minecarts to use ComplexVector

--- aoc2018/day13/minecarts.py
+++ aoc2018/day13/minecarts.py
@@ -1,4 +1,4 @@
-from functools import total_ordering
+from complexvector import ComplexVector
 
 NORTH = '^'
 SOUTH = 'v'
@@ -14,80 +14,50 @@ RIGHT = 1j
 STRAIGHT = 1
 TURNS = (LEFT, STRAIGHT, RIGHT)
 
-def direction(char):
-    switcher = {
-        NORTH : -1j,
-        SOUTH : 1j,
-        EAST : 1,
-        WEST : -1
-    }
-    return switcher.get(char)
-
-#implementing carts as a complex vector
-#y is flipped, so -j is up, and j is down
-@total_ordering
-class Cart:
-    def __init__(self, name, location, heading):
-        assert heading != 0j
-        self.name = name
-        self.location = location
-        self.direction = heading
+class Minecart(ComplexVector):
+    def __init__(self, magnitude, direction):
+        ComplexVector.__init__(self, magnitude, direction)
         self.next_turn = 0
-
-    def move(self):
-        self.location += self.direction
-
-    def turn(self, rotation):
-        assert rotation != 0j
-        self.direction *= rotation
 
     def update_turn_behavior(self):
         self.next_turn = (self.next_turn + 1) % 3
-
-    def __repr__(self):
-        return 'Cart {name} at {p} going {dir}'.format(
-            name=self.name,
-            p=self.location,
-            dir=self.direction)
-
-    def __lt__(self, other):
-        if self.location.imag == other.location.imag:
-            return self.location.real < other.location.real
-        return self.location.imag < other.location.imag
 
 class Minecarts:
     def __init__(self, file_name):
         file = open(file_name, 'r')
         lines = [x.rstrip() for x in file.readlines()]
         file.close()
+        self.track = []
+        self.carts = []
         self.generate_track(lines)
         self.crashes = []
 
     def generate_track(self, lines):
-        self.track = []
-        self.carts = []
-        i = 0
-        for line_enum in enumerate(lines):
-            to_process = list(line_enum[1])
-            for char_enum in enumerate(to_process):
-                char = char_enum[1]
-                if char in DIRECTIONS:
+        for row, line in enumerate(lines):
+            to_process = list(line)
+            for col, val in enumerate(to_process):
+                if val in DIRECTIONS:
                     #make a cart
-                    point = (line_enum[0] * 1j + char_enum[0])
-                    self.carts.append(Cart(i, point, direction(char)))
-                    i += 1
+                    magnitude = row + col * 1j
+                    direction = facing(val)
+                    self.carts.append(Minecart(magnitude, direction))
                     #replace the track with the proper representation
-                    if char in (EAST, WEST):
-                        to_process[char_enum[0]] = HORIZONTAL
-                    elif char in (SOUTH, NORTH):
-                        to_process[char_enum[0]] = VERTICAL
+                    if val in [NORTH, SOUTH]:
+                        to_process[col] = VERTICAL
+                    if val in [EAST, WEST]:
+                        to_process[col] = HORIZONTAL
             self.track.append(to_process)
 
     def run(self):
         while len(self.carts) > 1:
-            self.carts.sort()
+            self.carts.sort(key=lambda vector:
+                            vector.magnitude.imag < vector.magnitude.imag
+                            if vector.magnitude.real == vector.magnitude.real
+                            else vector.magnitude.real < vector.magnitude.real)
+            print('---')
             for cart in self.carts:
-                cart.move()
+                print(cart)
+                cart.translate(1)
                 self.check_for_crash(cart)
                 self.turn_cart(cart)
         if self.carts:
@@ -96,19 +66,19 @@ class Minecarts:
         return None
 
     def turn_cart(self, cart):
-        loc = location(cart)
-        track = self.track[loc[1]][loc[0]]
+        row, col = location(cart)
+        track = self.track[row][col]
         if track in CURVES:
-            cart.turn(take_curve(cart.direction, track))
+            cart.rotate(take_curve(cart.direction, track))
         elif track == JUNCTION:
-            cart.turn(TURNS[cart.next_turn])
+            cart.rotate(TURNS[cart.next_turn])
             cart.update_turn_behavior()
 
     def check_for_crash(self, cart):
         for other_cart in [c for c in self.carts if c != cart]:
-            if cart.location == other_cart.location:
+            if cart.magnitude == other_cart.magnitude:
                 print('CRASH at {loc}'.format(
-                    loc=cart.location))
+                    loc=cart.magnitude))
                 #Hack, implementation uses x as row and y as column
                 #This is reverse of the problem spec
                 self.crashes.append(location(cart))
@@ -118,8 +88,17 @@ class Minecarts:
                     size=len(self.carts)))
                 return
 
+def facing(char):
+    switcher = {
+        NORTH : -1,
+        SOUTH : 1,
+        EAST : 1j,
+        WEST : -1j
+    }
+    return switcher.get(char)
+
 def location(last_cart):
-    return int(last_cart.location.real), int(last_cart.location.imag)
+    return int(last_cart.magnitude.real), int(last_cart.magnitude.imag)
 
 def take_curve(heading, curve):
     if curve == '\\':
@@ -132,11 +111,12 @@ def take_curve(heading, curve):
 
 LINE = Minecarts('line.txt')
 LINE.run()
-assert LINE.crashes[0] == (0, 3)
+assert LINE.crashes[0] == (3, 0)
 
 SAMPLE = Minecarts('sample.txt')
 SAMPLE.run()
-assert SAMPLE.crashes[0] == (7, 3)
+print(SAMPLE.crashes)
+assert SAMPLE.crashes[0] == (3, 7)
 
 PROBLEM = Minecarts('input.txt')
 LAST_CART = PROBLEM.run()
@@ -145,20 +125,20 @@ print('Answer to Part 1: {ans}'.format(
 
 SAMPLE = Minecarts('sample_pt2.txt')
 LAST_CART = SAMPLE.run()
-assert SAMPLE.crashes[0] == (2, 0)
-assert SAMPLE.crashes[1] == (2, 4)
-assert SAMPLE.crashes[2] == (6, 4)
-assert SAMPLE.crashes[3] == (2, 4)
-assert LAST_CART == (6, 4)
+assert SAMPLE.crashes[0] == (0, 2)
+assert SAMPLE.crashes[1] == (4, 2)
+assert SAMPLE.crashes[2] == (4, 6)
+assert SAMPLE.crashes[3] == (4, 2)
+assert LAST_CART == (4, 6)
 
 CRASHES = PROBLEM.crashes
-assert CRASHES[0] == (28, 107)
-assert CRASHES[1] == (47, 103)
-assert CRASHES[2] == (91, 85)
-assert CRASHES[3] == (6, 80)
-assert CRASHES[4] == (39, 54)
-assert CRASHES[5] == (134, 37)
-assert CRASHES[6] == (26, 100)
+assert CRASHES[0] == (107, 28)
+assert CRASHES[1] == (103, 47)
+assert CRASHES[2] == (85, 91)
+assert CRASHES[3] == (80, 6)
+assert CRASHES[4] == (54, 39)
+assert CRASHES[5] == (37, 134)
+assert CRASHES[6] == (100, 26)
 assert CRASHES[7] == (-1, -1)
 print('Answer to Part 2: {ans}'.format(
     ans=LAST_CART))

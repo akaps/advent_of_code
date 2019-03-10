7eda4e4d260542526980fdeff57390277b27ecec
Ari <ari.kaps@gmail.com>
2019-03-10 10:06:57 -0700

    solution to 2016 day 3

--- aoc2016/day03/triangles.py
+++ aoc2016/day03/triangles.py
@@ -17,3 +17,15 @@ for line in PROBLEM:
     if is_triangle(int(a), int(b), int(c)):
         TOTAL += 1
 utils.pretty_print_answer(1, TOTAL)
+
+TOTAL = 0
+for line1, line2, line3 in zip(*[iter(PROBLEM)]*3):
+    a1, a2, a3 = re.split(r' +', line1)
+    b1, b2, b3 = re.split(r' +', line2)
+    c1, c2, c3 = re.split(r' +', line3)
+    triangles = [(a1, b1, c1), (a2, b2, c2), (a3, b3, c3)]
+    for i in range(3):
+        a, b, c = triangles[i]
+        if is_triangle(int(a), int(b), int(c)):
+            TOTAL += 1
+utils.pretty_print_answer(2, TOTAL)

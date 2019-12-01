04e41f0c0f50a7afd69e61c7f5bf19d81273d368
Ari <ari.kaps@gmail.com>
2019-11-30 21:33:16 -0800

    day 1 solution using utils module
    providing meaningful method names for part_1, part_2
    addings asserts for part 1
    call into utils for reading input

--- aoc2019/day01/fuel.py
+++ aoc2019/day01/fuel.py
@@ -1,4 +1,4 @@
-#import utils #TODO: import utils for pretty print
+import utils
 
 def calculate_fuel(mass):
     return mass //3 - 2
@@ -11,30 +11,29 @@ def calculate_log_fuel(mass):
         current = calculate_fuel(current)
     return total
 
-def part_1(mass_list):
+def simple_fuel(mass_list):
     total = 0
     for mass in mass_list:
-        total += calculate_fuel(int(mass))
-
-    #utils.pretty_print_answer(1, total)
+        total += calculate_fuel(mass)
     assert total == 3295539
-    print('part 1: {ans}'.format(ans=total))
+    utils.pretty_print_answer(1, total)
 
-def part_2(mass_list):
+def realistic_fuel(mass_list):
     total = 0
     for mass in mass_list:
-        total += calculate_log_fuel(int(mass))
+        total += calculate_log_fuel(mass)
+    assert total == 4940441
+    utils.pretty_print_answer(2, total)
 
-    print('part 2: {ans}'.format(ans=total))
-
-file = open('input.txt', 'r')
-mass_list = file.readlines()
-file.close()
-
-part_1(mass_list)
+assert calculate_fuel(12) == 2
+assert calculate_fuel(14) == 2
+assert calculate_fuel(1969) == 654
+assert calculate_fuel(100756) == 33583
 
 assert calculate_log_fuel(14) == 2
 assert calculate_log_fuel(1969) == 966
 assert calculate_log_fuel(100756) == 50346
 
-part_2(mass_list)
+mass_list = [int(x) for x in utils.read_lines('input.txt')]
+simple_fuel(mass_list)
+realistic_fuel(mass_list)

39a5462608ef79949032cb7f6286ae194a927da3
Ari <ari.kaps@gmail.com>
2019-12-03 21:59:02 -0800

    refactoring day 3
    changed points list into a dictionary
    values were steps
    avoid repeating points to get smallest step count
    naming current current point and origin to avoid magic numbers
    got rid of intersections list as we are fast now :)
    removed debug printing
    prettily formatted results
    asserts for solutions

--- aoc2019/day03/wires.py
+++ aoc2019/day03/wires.py
@@ -2,12 +2,11 @@ import re
 import utils
 
 DELIMETER = r','
-INTERSECTION_DISTS = [248, 367, 1112, 1948, 2228]
+ORIGIN = (0, 0)
 
 def generate_points(wire):
-    intersections = {}
     row = col = 0
-    points = []
+    points = {}
     current_steps = 0
     for instruction in wire:
         direction, current_distance = instruction[:1], int(instruction[1:])
@@ -21,33 +20,34 @@ def generate_points(wire):
                 row += 1
             if direction == 'L':
                 row -= 1
-            current_distance = utils.manhattan_distance((row, col), (0, 0))
-            if current_distance in INTERSECTION_DISTS:
-                intersections[current_distance] = current_steps
-            points.append((row, col))
-    return points, intersections
+            current_point = (row, col)
+            current_distance = utils.manhattan_distance(current_point, ORIGIN)
+            if current_point not in points:
+                points[(row, col)] = current_steps
+    return points
 
 INPUT = utils.read_lines('input.txt')
 WIRE_A = re.split(DELIMETER, INPUT[0])
 WIRE_B = re.split(DELIMETER, INPUT[1])
 
-POINTS_A, INTERSECTIONS_A = generate_points(WIRE_A)
-print('wire a done')
-POINTS_B, INTERSECTIONS_B = generate_points(WIRE_B)
-print('wire b done')
+POINTS_A = generate_points(WIRE_A)
+POINTS_B = generate_points(WIRE_B)
 
 LOCATION = None
 SHORTEST_TRIP = -1
 for point in POINTS_A:
     if point in POINTS_B:
         distance = utils.manhattan_distance(point, (0, 0))
-        print('match! {pt} {distance}'.format(pt=point, distance=distance))
-        steps = POINTS_A.index(point) + 1 + POINTS_B.index(point) + 1
-        print(steps)
+        steps = POINTS_A[point] + POINTS_B[point]
+        print('match @ {pt}\tdist={distance}\tsteps={steps}'.format(pt=point,
+                                                                    distance=distance,
+                                                                    steps=steps))
         if SHORTEST_TRIP < 0 or steps < SHORTEST_TRIP:
             SHORTEST_TRIP = steps
         if not LOCATION or distance < LOCATION:
             LOCATION = distance
 
+assert LOCATION == 248
+assert SHORTEST_TRIP == 28580
 utils.pretty_print_answer(1, LOCATION)
 utils.pretty_print_answer(2, SHORTEST_TRIP)

502441da56e189b3e4cf67d6391bdaf33d0b9fca
Ari <ari.kaps@gmail.com>
2019-12-06 20:54:56 -0800

    solution to 2019 day 6 sample
    for input, need to support disconnected graphs

---
+++ aoc2019/day06/orbits.py
@@ -0,0 +1,34 @@
+import utils
+
+CENTER = 'COM'
+DELIMETER = ')'
+
+class Constellation:
+    def __init__(self, input_file):
+        self.orbits = {}
+        self.initialize_orbits(utils.read_lines(input_file))
+
+    def initialize_orbits(self, orbit_pairs):
+        self.orbits[CENTER] = []
+        for pair in orbit_pairs:
+            key, val = pair.split(DELIMETER)
+            self.orbits[val] = []
+            if key in self.orbits:
+                self.orbits[key].append(val)
+
+    def total_distance(self):
+        return self._total_distance(CENTER, 0)
+
+    def _total_distance(self, current_node, current_depth):
+        total = current_depth
+        for child in self.orbits[current_node]:
+            total += self._total_distance(child, current_depth + 1)
+        return total
+
+SAMPLE = Constellation('sample.txt')
+print(SAMPLE.orbits)
+DISTANCE = SAMPLE.total_distance()
+assert DISTANCE == 42
+
+PROBLEM = Constellation('input.txt')
+utils.pretty_print_answer(1, PROBLEM.total_distance())

aa4719cd559f5c7215f35fa6d07dd5a66da84602
Ari <ari.kaps@gmail.com>
2019-12-11 12:00:43 -0800

    solution to day 6 part 1
    sample provided nodes in order while input does not
    properly initialize state for each node in this case
    happy accident, we will need parent data for each node for part 2

--- aoc2019/day06/orbits.py
+++ aoc2019/day06/orbits.py
@@ -3,25 +3,43 @@ import utils
 CENTER = 'COM'
 DELIMETER = ')'
 
+class Star:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def __repr__(self):
+        return '{data} has parent {parent} and children {children}'.format(
+            data=self.data,
+            parent=self.parent,
+            children=self.children
+        )
+
 class Constellation:
     def __init__(self, input_file):
-        self.orbits = {}
+        self.stars = {}
         self.initialize_orbits(utils.read_lines(input_file))
 
     def initialize_orbits(self, orbit_pairs):
-        self.orbits[CENTER] = []
         for pair in orbit_pairs:
-            key, val = pair.split(DELIMETER)
-            self.orbits[val] = []
-            if key in self.orbits:
-                self.orbits[key].append(val)
+            center, orbiter = pair.split(DELIMETER)
+            if center not in self.stars:
+                self.stars[center] = Star(center)
+            self.stars[center].add_child(orbiter)
+            if orbiter not in self.stars:
+                self.stars[orbiter] = Star(orbiter)
+            self.stars[orbiter].parent = center
 
     def total_distance(self):
         return self._total_distance(CENTER, 0)
 
     def _total_distance(self, current_node, current_depth):
         total = current_depth
-        for child in self.orbits[current_node]:
+        for child in self.stars[current_node].children:
             total += self._total_distance(child, current_depth + 1)
         return total
 

d60cf23e808c44f87192fb3ceb4563318ea2d96f
Ari <ari.kaps@gmail.com>
2019-12-12 22:48:58 -0800

    solution to day 13 part 2
    for understanding the problem, make a screen
    include positions for quick lookup
    I really wanna get rid of ball_pos_prev, but
    you need to never do neutral past the first time

--- aoc2019/day13/breakout.py
+++ aoc2019/day13/breakout.py
@@ -1,5 +1,5 @@
 import utils
-from aoc2019.int_code import IntCode
+from aoc2019.int_code import IntCode, MissingInputError
 
 EMPTY = 0
 WALL = 1
@@ -15,6 +15,10 @@ TILE_REPS = {
     BALL: 'O'
 }
 
+NEUTRAL = 0
+LEFT = -1
+RIGHT = 1
+
 SCREEN_WIDTH = 43
 SCREEN_HEIGHT = 21
 
@@ -26,17 +30,55 @@ class Breakout:
         self.tiles = {}
         self.model = IntCode(input_file)
         self.score = 0
+        self.ball_pos_prev = None
+        self.ball_pos_curr = None
+        self.paddle_pos = None
 
-    def update_tile(self, x, y, tile):
-        if x == -1:
+    def update_tile(self, left_right, up_down, tile):
+        if left_right == -1:
             self.score = tile
         else:
-            self.tiles[(x, y)] = tile
+            if tile == HORIZ_PADDLE:
+                self.paddle_pos = (left_right, up_down)
+            elif tile == BALL:
+                self.ball_pos_prev = self.ball_pos_curr
+                self.ball_pos_curr = (left_right, up_down)
+            self.tiles[(left_right, up_down)] = tile
 
-    def run(self):
+    def generate_next_move(self):
+        if not self.ball_pos_prev:
+            return NEUTRAL
+        if self.paddle_pos > self.ball_pos_curr:
+            return LEFT
+        return RIGHT
+
+    def run(self, debug=False):
+        try:
+            result = self.model.run_program()
+            self.update_tiles(result)
+        except MissingInputError:
+            result = self.model.output
+            self.model.output = []
+            self.update_tiles(result)
+            while self.model.registers[self.model.instruction_pointer] != self.model.STOP:
+                next_move = None
+                if debug:
+                    print(self)
+                    next_move = int(input('next_move'))
+                else:
+                    next_move = self.generate_next_move()
+                try:
+                    result = self.model.run_program([next_move])
+                except MissingInputError:
+                    result = self.model.output
+                    self.model.output = []
+                    self.update_tiles(result)
         result = self.model.run_program()
-        for x, y, z in parse_results(result):
-            self.update_tile(x, y, z)
+        self.update_tiles(result)
+
+    def update_tiles(self, results):
+        for left_right, up_down, tile in parse_results(results):
+            self.update_tile(left_right, up_down, tile)
 
     def count_tiles(self, tile_type):
         count = 0
@@ -47,10 +89,10 @@ class Breakout:
 
     def __repr__(self):
         rows = []
-        for y in range(SCREEN_HEIGHT):
+        for up_down in range(SCREEN_HEIGHT):
             current_row = []
-            for x in range(SCREEN_WIDTH):
-                coord = (x, y)
+            for left_right in range(SCREEN_WIDTH):
+                coord = (left_right, up_down)
                 tile = EMPTY
                 if coord in self.tiles:
                     tile = self.tiles[coord]
@@ -74,3 +116,10 @@ assert SAMPLE.count_tiles(HORIZ_PADDLE) == 1
 PROBLEM = Breakout('input.txt')
 PROBLEM.run()
 utils.pretty_print_answer(1, PROBLEM.count_tiles(BLOCK))
+
+PROBLEM = Breakout('input.txt')
+PROBLEM.model.registers[0] = 2
+PROBLEM.run()
+assert PROBLEM.count_tiles(BLOCK) == 0
+assert PROBLEM.score == 13581
+utils.pretty_print_answer(2, PROBLEM.score)

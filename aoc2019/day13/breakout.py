c6666f88810c96200d8004920c43481927a03f9b
Ari <ari.kaps@gmail.com>
2019-12-12 22:08:44 -0800

    adding repr to breakout for part 2
    clean visualization of screen state for play

--- aoc2019/day13/breakout.py
+++ aoc2019/day13/breakout.py
@@ -7,6 +7,17 @@ BLOCK = 2
 HORIZ_PADDLE = 3
 BALL = 4
 
+TILE_REPS = {
+    EMPTY: ' ',
+    WALL: '#',
+    BLOCK: 'X',
+    HORIZ_PADDLE: '_',
+    BALL: 'O'
+}
+
+SCREEN_WIDTH = 43
+SCREEN_HEIGHT = 21
+
 def parse_results(result):
     return [(result[i], result[i + 1], result[i + 2]) for i in range(0, len(result), 3)]
 
@@ -14,23 +25,39 @@ class Breakout:
     def __init__(self, input_file):
         self.tiles = {}
         self.model = IntCode(input_file)
+        self.score = 0
 
-    def add_tile(self, x, y, tile):
-        self.tiles[(x, y)] = tile
+    def update_tile(self, x, y, tile):
+        if x == -1:
+            self.score = tile
+        else:
+            self.tiles[(x, y)] = tile
 
     def run(self):
         result = self.model.run_program()
         for x, y, z in parse_results(result):
-            self.add_tile(x, y, z)
+            self.update_tile(x, y, z)
 
     def count_tiles(self, tile_type):
         count = 0
         for tile in self.tiles.values():
-            print(tile)
             if tile == tile_type:
                 count += 1
-        print(count)
         return count
+
+    def __repr__(self):
+        rows = []
+        for y in range(SCREEN_HEIGHT):
+            current_row = []
+            for x in range(SCREEN_WIDTH):
+                coord = (x, y)
+                tile = EMPTY
+                if coord in self.tiles:
+                    tile = self.tiles[coord]
+                current_row.append(TILE_REPS[tile])
+            rows.append(''.join(current_row))
+        rows.append('score: {score}'.format(score=self.score))
+        return '\n'.join(rows)
 
 RESULT = [1, 2, 3, 6, 5, 4]
 PARSED = parse_results(RESULT)
@@ -40,7 +67,7 @@ assert PARSED[1] == (6, 5, 4)
 
 SAMPLE = Breakout('input.txt')
 for x, y, z in PARSED:
-    SAMPLE.add_tile(x, y, z)
+    SAMPLE.update_tile(x, y, z)
 assert SAMPLE.count_tiles(BALL) == 1
 assert SAMPLE.count_tiles(HORIZ_PADDLE) == 1
 

7aa37fa1e3631cfc8e852cdd7016dfda6b5e8f83
Ari <ari.kaps@gmail.com>
2019-12-20 23:11:35 -0800

    solution to day 21 part 2

--- aoc2019/day21/springdroid.py
+++ aoc2019/day21/springdroid.py
@@ -3,4 +3,10 @@ from aoc2019.springscript import SpringScript
 
 PROBLEM = SpringScript('input.txt', 'walk_program.txt')
 ANSWER = PROBLEM.run()
+assert ANSWER == 19354173
 utils.pretty_print_answer(1, ANSWER)
+
+PROBLEM = SpringScript('input.txt', 'run_program.txt')
+ANSWER = PROBLEM.run()
+assert ANSWER == 1145849660
+utils.pretty_print_answer(1,  ANSWER)

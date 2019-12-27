f1415ac4608ff0bbd41ee3df42f94ce29932c335
Ari <ari.kaps@gmail.com>
2019-12-26 23:18:41 -0800

    solution to dya 7 part 2
    a few problems:
    did not read instructions.
    to start the feedback loop, you pass in the phase AND zero to amp[0]
    initializing is different than the successive calls
    handle this in a separate method
    need to reinitialize amps between phase combinations
    
    need to refactor on a machine that can import utils/int_code

--- aoc2019/day07/amplifiers.py
+++ aoc2019/day07/amplifiers.py
@@ -1,6 +1,5 @@
 import itertools
-import utils
-from aoc2019.int_code import IntCode
+from int_code import IntCode, MissingInputError
 
 class Amplifiers:
     NUM_AMPLIFIERS = 5
@@ -21,6 +20,45 @@ class Amplifiers:
                 largest_output = output
         return largest_output
 
+    def is_halted(self, amplifier):
+        return amplifier.registers[amplifier.instruction_pointer] == 99
+
+    def initialize_amplifiers(self, combination):
+        previous_result = [0]
+        for index, phase in enumerate(combination):
+            previous_result.insert(0, phase)
+            try:
+                previous_result = self.amplifiers[index].run_program(previous_result)
+            except MissingInputError:
+                previous_result = self.amplifiers[index].output
+                self.amplifiers[index].output = []
+        return previous_result
+
+    def run_feedback_loop(self, combination):
+        self.reinitialize_amplifiers()
+        previous_result = self.initialize_amplifiers(combination)
+        while not self.is_halted(self.amplifiers[-1]):
+            for amplifier in self.amplifiers:
+                try:
+                    previous_result = amplifier.run_program(previous_result)
+                except MissingInputError:
+                    previous_result = amplifier.output
+                    amplifier.output = []
+        return previous_result[0]
+
+    def reinitialize_amplifiers(self):
+        for amplifier in self.amplifiers:
+            amplifier.reinitialize()
+
+    def largest_feedback_loop(self):
+        combinations = itertools.permutations(range(5, 10))
+        largest_output = None
+        for combination in combinations:
+            output_val = self.run_feedback_loop(combination)
+            if not largest_output or output_val > largest_output:
+                largest_output = output_val
+        return largest_output
+
 SAMPLE1 = Amplifiers('sample1.txt')
 assert SAMPLE1.find_largest_output() == 43210
 
@@ -31,4 +69,7 @@ SAMPLE3 = Amplifiers('sample3.txt')
 assert SAMPLE3.find_largest_output() == 65210
 
 PROBLEM = Amplifiers('input.txt')
-utils.pretty_print_answer(1, PROBLEM.find_largest_output())
+print('Answer to part 1:', PROBLEM.find_largest_output())
+
+PROBLEM.reinitialize_amplifiers()
+print('Answer to part 2:', PROBLEM.largest_feedback_loop())

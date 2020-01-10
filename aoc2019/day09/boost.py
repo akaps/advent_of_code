982305809dea3da8356a0a888f8a6162f32b59f7
Ari <ari.kaps@gmail.com>
2020-01-10 15:28:41 -0800

    encapsulating boost in a main method
    day09_test validates int_code, so we don't import boost
    however, it's good practice and I saw it during review

--- aoc2019/day09/boost.py
+++ aoc2019/day09/boost.py
@@ -1,15 +1,19 @@
 import utils
 from aoc2019.int_code import IntCode
 
-COMPUTER = IntCode('input.txt')
-PROGRAM = COMPUTER.load_program()
-PROGRAM.send(None)
-RESULT = PROGRAM.send(1)
-assert len(RESULT) == 1, 'opcodes failed according to results {res}'.format(res=RESULT)
-utils.pretty_print_answer(1, RESULT[0])
+def main():
+    computer = IntCode('input.txt')
+    program = computer.load_program()
+    program.send(None)
+    result = program.send(1)
+    assert len(result) == 1, 'opcodes failed according to results {res}'.format(res=result)
+    utils.pretty_print_answer(1, result[0])
 
-PROGRAM = COMPUTER.load_program()
-PROGRAM.send(None)
-RESULT = PROGRAM.send(2)
-assert len(RESULT) == 1, 'unexpected additional output {res}'.format(res=RESULT)
-utils.pretty_print_answer(2, RESULT[0])
+    program = computer.load_program()
+    program.send(None)
+    result = program.send(2)
+    assert len(result) == 1, 'unexpected additional output {res}'.format(res=result)
+    utils.pretty_print_answer(2, result[0])
+
+if __name__ == '__main__':
+    main()

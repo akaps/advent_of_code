3698cc84c8b0d7e944d3338a4f175cb4d796cf98
Ari <ari.kaps@gmail.com>
2020-01-10 01:09:54 -0800

    Refactoring running a program
    previously we used exception handling to catch when there
    wasn't enough input
    This was generally agreed by everyone as a very bad move
    insteaad, treat program execution with a generator function
    yield results back when more input is required, and return
    the register state at the end
    currently solves day 2 AND NOTHING ELSE

--- aoc2019/day02/program_alarm.py
+++ aoc2019/day02/program_alarm.py
@@ -4,20 +4,20 @@ from aoc2019.int_code import IntCode
 def find_noun_verb(computer, target):
     for noun in range(100):
         for verb in range(100):
-            COMPUTER.reinitialize()
-            COMPUTER.registers[1] = noun
-            COMPUTER.registers[2] = verb
-            computer.run_program()
-            if computer.registers[0] == target:
+            computer.initial_state[1] = noun
+            computer.initial_state[2] = verb
+            generator = computer.load_program()
+            result = generator.send(None)[0]
+            if result == target:
                 return 100 * noun + verb
     assert False, 'Unreachable'
     return -1
 
 COMPUTER = IntCode('input.txt')
-COMPUTER.registers[1] = 12
-COMPUTER.registers[2] = 2
-COMPUTER.run_program()
-ANSWER = COMPUTER.registers[0]
+COMPUTER.initial_state[1] = 12
+COMPUTER.initial_state[2] = 2
+GENERATOR = COMPUTER.load_program()
+ANSWER = GENERATOR.send(None)[0]
 assert ANSWER == 2692315
 utils.pretty_print_answer(1, ANSWER)
 

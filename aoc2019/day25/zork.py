158b49b0f8078f01a2f3cb8709091530e1787d8b
Ari <ari.kaps@gmail.com>
2020-01-12 01:26:07 -0800

    removing writing answer from day 25
    Previously needed to write last room to file because
    we were running all combinations of items at once
    with a reduced answer text file and the ability to exit,
    you can read an answer in console and overwrite
    the file if exiting early
    removing the write feature as it's no longer necessary
    
    minor nit: do not change the input_string conditional to
    make the constant more understandable

--- aoc2019/day25/zork.py
+++ aoc2019/day25/zork.py
@@ -2,7 +2,7 @@ import utils
 from aoc2019.int_code import IntCode
 
 NEWLINE = '\n'
-EXIT = 'exit\n'
+EXIT = 'exit'
 
 def main():
     program = IntCode('input.txt').load_program()
@@ -10,15 +10,11 @@ def main():
     input_string = None
     while input_string != EXIT:
         input_string = input(utils.translate_to_chars(output) + NEWLINE)
-        input_string += NEWLINE
         try:
-            for char in input_string:
+            for char in input_string + NEWLINE:
                 output = program.send(ord(char))
         except StopIteration:
             break
-    file = open('answer_pt_1.txt', 'w')
-    file.write(utils.translate_to_chars(output))
-    file.close()
 
 if __name__ == '__main__':
     main()

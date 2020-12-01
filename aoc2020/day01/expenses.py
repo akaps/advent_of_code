66c36b51b426e0642eecb37a1a372214d3e77bc9
Ari <ari.kaps@gmail.com>
2020-11-30 21:17:48 -0800

    refactoring to avoid duplicate load

--- aoc2020/day01/expenses.py
+++ aoc2020/day01/expenses.py
@@ -1,16 +1,12 @@
 import utils
 
-def product_of_20(file_name):
-    lines = utils.read_lines(file_name)
-    lines = [int(line) for line in lines]
+def product_of_20(lines):
     for index, val in enumerate(lines):
         for j in range(index + 1, len(lines)):
             if val + lines[j] == 2020:
                 return val * lines[j]
 
-def product_of_20_for_3(file_name):
-    lines = utils.read_lines(file_name)
-    lines = [int(line) for line in lines]
+def product_of_20_for_3(lines):
     for index, val in enumerate(lines):
         for j in range(index + 1, len(lines)):
             for k in range(j + 1, len(lines)):
@@ -18,8 +14,10 @@ def product_of_20_for_3(file_name):
                     return val * lines[j] * lines[k]
 
 def main():
-    utils.pretty_print_answer(1, product_of_20('input.txt'))
-    utils.pretty_print_answer(2, product_of_20_for_3('input.txt'))
+    lines = utils.read_lines('input.txt')
+    lines = [int(line) for line in lines]
+    utils.pretty_print_answer(1, product_of_20(lines))
+    utils.pretty_print_answer(2, product_of_20_for_3(lines))
 
 if __name__ == '__main__':
     main()

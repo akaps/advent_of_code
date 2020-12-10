f9793db5febe8d641ce6bbaadd15806772d9ccfd
Ari <ari.kaps@gmail.com>
2020-12-09 19:57:48 -0800

    simplifying VirtualMachine with VmState
    Consistent calling convention to execute and run

--- aoc2020/day08/halting.py
+++ aoc2020/day08/halting.py
@@ -1,26 +1,24 @@
 from collections import defaultdict
 import copy
 import utils
-from aoc2020.virtual_machine import VirtualMachine, JUMP, NOP, ACC, OPCODE
+import aoc2020.virtual_machine as vm
 
 def run_til_repeat(virtual_machine):
-    instruction_pointer = 0
-    accumulator = 0
+    vm_state = vm.VmState()
     ips = defaultdict(lambda: False)
     infinite = False
-    while instruction_pointer < len(virtual_machine.instructions) and not ips[instruction_pointer]:
-        ips[instruction_pointer] = True
-        instruction = virtual_machine.instructions[instruction_pointer]
-        accumulator, instruction_pointer = virtual_machine.execute(
-            instruction, accumulator, instruction_pointer)
-        if instruction_pointer in ips:
+    while vm_state.instruction_pointer < len(virtual_machine.instructions) and not ips[vm_state.instruction_pointer]:
+        ips[vm_state.instruction_pointer] = True
+        instruction = virtual_machine.instructions[vm_state.instruction_pointer]
+        vm_state = vm.execute(instruction, vm_state)
+        if vm_state.instruction_pointer in ips:
             infinite = True
-    return accumulator, infinite
+    return vm_state.accumulator, infinite
 
 def find_non_accs(instructions):
     result = []
     for index, instruction in enumerate(instructions):
-        if instruction[OPCODE] != ACC:
+        if instruction[vm.OPCODE] != vm.ACC:
             result.append((index, instruction))
     return result
 
@@ -30,10 +28,10 @@ def part_2(virtual_machine):
     non_accs.reverse()
     for index, instruction in non_accs:
         modified_instructions = copy.deepcopy(virtual_machine.instructions)
-        if JUMP in instruction[OPCODE]:
-            modified_instructions[index][OPCODE] = NOP
-        elif NOP in instruction[OPCODE]:
-            modified_instructions[index][OPCODE] = JUMP
+        if vm.JUMP in instruction[vm.OPCODE]:
+            modified_instructions[index][vm.OPCODE] = vm.NOP
+        elif vm.NOP in instruction[vm.OPCODE]:
+            modified_instructions[index][vm.OPCODE] = vm.JUMP
         else:
             assert False, 'Unexpected opcode {op}'.format(op=instruction)
         virtual_machine.instructions = modified_instructions
@@ -46,7 +44,7 @@ def part_2(virtual_machine):
     return -1
 
 def main():
-    virtual_machine = VirtualMachine('aoc2020/day08/input.txt')
+    virtual_machine = vm.VirtualMachine('aoc2020/day08/input.txt')
     utils.pretty_print_answer(1, run_til_repeat(virtual_machine)[0])
     utils.pretty_print_answer(2, part_2(virtual_machine))
 
--- aoc2020/virtual_machine.py
+++ aoc2020/virtual_machine.py
@@ -10,6 +10,25 @@ ARGUMENT = 'value'
 
 INSTRUCTION_REGEX = r'(?P<op>acc|jmp|nop) (?P<number>(\+|-)\d+)'
 
+def execute(instruction, vm_state):
+    opcode = instruction[OPCODE]
+    value = instruction[ARGUMENT]
+    if opcode == ACC:
+        vm_state.accumulator += value
+        vm_state.instruction_pointer += 1
+    elif opcode == JUMP:
+        vm_state.instruction_pointer += value
+    elif opcode == NOP:
+        vm_state.instruction_pointer += 1
+    else:
+        assert False, 'Unexpected opcode {opcode}'.format(opcode=opcode)
+    return vm_state
+
+class VmState:
+    def __init__(self, accumulator=0, instruction_pointer=0):
+        self.accumulator = accumulator
+        self.instruction_pointer = instruction_pointer
+
 class VirtualMachine:
     def __init__(self, file_name):
         lines = utils.read_lines(file_name)
@@ -21,24 +40,9 @@ class VirtualMachine:
             value = int(match.groups()[1])
             self.instructions.append({OPCODE: opcode, ARGUMENT: value})
 
-    def execute(self, instruction, accumulator, instruction_pointer):
-        opcode = instruction[OPCODE]
-        value = instruction[ARGUMENT]
-        if opcode == ACC:
-            accumulator += value
-            instruction_pointer += 1
-        elif opcode == JUMP:
-            instruction_pointer += value
-        elif opcode == NOP:
-            instruction_pointer += 1
-        else:
-            assert False, 'Unexpected opcode {opcode}'.format(opcode=opcode)
-        return accumulator, instruction_pointer
-
     def run(self, accumulator=0, start_ip=0):
-        instruction_pointer = start_ip
-        while instruction_pointer < len(self.instructions):
-            next_instruction = self.instructions[instruction_pointer]
-            accumulator, instruction_pointer = self.execute(
-                next_instruction, accumulator, instruction_pointer)
-        return accumulator
+        vm_state = VmState(accumulator, start_ip)
+        while vm_state.instruction_pointer < len(self.instructions):
+            next_instruction = self.instructions[vm_state.instruction_pointer]
+            vm_state = execute(next_instruction, vm_state)
+        return vm_state.accumulator

1c8d729238a009c502db66468f6977200e112720
Ari <ari.kaps@gmail.com>
2020-12-10 22:00:17 -0800

    solution to part 2
    have an optimization in mind, but both finish reasonably

--- aoc2020/day11/seats.py
+++ aoc2020/day11/seats.py
@@ -4,32 +4,58 @@ OCCUPIED = '#'
 EMPTY = 'L'
 FLOOR = '.'
 
+DIRECTIONS = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1)
+]
+
 class Seats:
     def __init__(self, file_name):
         self.seats = utils.read_matrix(file_name)
-        self.states = []
 
     def generate_stable(self):
+        states = []
         count = self.count_occupied()
-        while count not in self.states:
-            self.states.append(count)
+        while count not in states:
+            states.append(count)
             self.seats, count = self.next_generation()
 
+    def generate_stable_queens(self):
+        states = []
+        count = self.count_occupied()
+        while count not in states:
+            states.append(count)
+            self.seats, count = self.next_generation_queens()
 
-    def in_bounds_not_identity(self, row, col):
+    def in_bounds(self, row, col):
         return 0 <= row < len(self.seats) and 0 <= col < len(self.seats[0])
-
 
     def count_adj(self, row, col):
         count = 0
-        for row_mod in range(-1, 2):
-            for col_mod in range(-1, 2):
-                if not row_mod == col_mod == 0:
-                    next_row = row + row_mod
-                    next_col = col + col_mod
-                    if (self.in_bounds_not_identity(next_row, next_col)
-                            and self.seats[next_row][next_col] == OCCUPIED):
-                        count += 1
+        for row_mod, col_mod in DIRECTIONS:
+            next_row = row + row_mod
+            next_col = col + col_mod
+            if (self.in_bounds(next_row, next_col)
+                    and self.seats[next_row][next_col] == OCCUPIED):
+                count += 1
+        return count
+
+    def count_adj_queens(self, row, col):
+        count = 0
+        for row_mod, col_mod in DIRECTIONS:
+            next_row = row + row_mod
+            next_col = col + col_mod
+            while self.in_bounds(next_row, next_col) and self.seats[next_row][next_col] == FLOOR:
+                next_row = next_row + row_mod
+                next_col = next_col + col_mod
+            if self.in_bounds(next_row, next_col) and self.seats[next_row][next_col] == OCCUPIED:
+                count += 1
         return count
 
     def next_generation(self):
@@ -43,6 +69,28 @@ class Seats:
                     continue
                 count = self.count_adj(row_index, col_index)
                 if seat == OCCUPIED and count >= 4:
+                    generation_row.append(EMPTY)
+                elif seat == EMPTY and count == 0:
+                    generation_row.append(OCCUPIED)
+                    num_occupied += 1
+                else:
+                    generation_row.append(seat)
+                    if seat == OCCUPIED:
+                        num_occupied += 1
+            generation.append(generation_row)
+        return generation, num_occupied
+
+    def next_generation_queens(self):
+        generation = []
+        num_occupied = 0
+        for row_index, row in enumerate(self.seats):
+            generation_row = []
+            for col_index, seat in enumerate(row):
+                if seat == FLOOR:
+                    generation_row.append(FLOOR)
+                    continue
+                count = self.count_adj_queens(row_index, col_index)
+                if seat == OCCUPIED and count >= 5:
                     generation_row.append(EMPTY)
                 elif seat == EMPTY and count == 0:
                     generation_row.append(OCCUPIED)
@@ -72,6 +120,10 @@ def main():
     seats = Seats('input.txt')
     seats.generate_stable()
     utils.pretty_print_answer(1, seats.count_occupied())
+    seats = Seats('input.txt')
+    seats.generate_stable_queens()
+    utils.pretty_print_answer(2, seats.count_occupied())
+
 
 if __name__ == "__main__":
     main()

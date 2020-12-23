08dfb58f8a8a57e09955acb859b1a3133c4ba9c7
Ari <ari.kaps@gmail.com>
2020-12-22 23:31:57 -0800

    slow solution to part 2
    Given 800 hours it should yield a result :(
    finding the 1 is a O(n) operation, convert to a dictionary
    to set that to constant time

--- aoc2020/day23/cups.py
+++ aoc2020/day23/cups.py
@@ -6,13 +6,19 @@ class Cup:
         self.next = None
 
 class Cups:
-    def __init__(self, input_str):
+    def __init__(self, input_str, is_big=False):
         input_list = [int(x) for x in input_str]
         self.current_cup = Cup(input_list[0])
         runner = self.current_cup
+        self.length = 9
         for val in input_list[1:]:
             runner.next = Cup(val)
             runner = runner.next
+        if is_big:
+            for val in range(10, 1000001):
+                runner.next = Cup(val)
+                runner = runner.next
+            self.length = 1000000
         runner.next = self.current_cup
 
     def extract(self):
@@ -29,9 +35,9 @@ class Cups:
         #pick up the next 3 cups:
         values, extracted = self.extract()
         #find next value, which is current_cup.value-1
-        destination = self.current_cup.value - 1 if self.current_cup.value > 1 else 9
+        destination = self.current_cup.value - 1 if self.current_cup.value > 1 else self.length
         while destination in values:
-            destination = destination - 1 if destination > 1 else 9
+            destination = destination - 1 if destination > 1 else self.length
         runner = self.current_cup
         while runner.value != destination:
             runner = runner.next
@@ -42,7 +48,9 @@ class Cups:
         self.current_cup = self.current_cup.next
 
     def play_game(self, num_rounds):
-        for _ in range(num_rounds):
+        for i in range(num_rounds):
+            if i % 1 == 0:
+                print('round', i)
             self.play_round()
 
     def cup_order(self):
@@ -55,12 +63,22 @@ class Cups:
             result.append(str(runner.value))
             runner = runner.next
         assert len(result) == 8
-        return ''.join(result)
+        return ' '.join(result)
+
+    def cup_hash(self):
+        runner = self.current_cup
+        while runner.value != 1:
+            runner = runner.next
+        return runner.next.value * runner.next.next.value
 
 def main():
     cups = Cups('253149867')
     cups.play_game(100)
     utils.pretty_print_answer(1, cups.cup_order())
+    cups = Cups('253149867', is_big=True)
+    cups.play_game(10000000)
+    print(cups.cup_order())
+    utils.pretty_print_answer(2, cups.cup_hash())
 
 if __name__ == "__main__":
     main()
--- aoc2020/day23/test_cups.py
+++ aoc2020/day23/test_cups.py
@@ -18,3 +18,27 @@ def test_round100():
     cups = Cups('389125467')
     cups.play_game(100)
     assert cups.cup_order() == '67384529'
+
+def test_cup_hash_start():
+    cups = Cups('389125467')
+    assert cups.cup_hash() == 10
+
+def test_cup_hash_1_round():
+    cups = Cups('389125467')
+    cups.play_round()
+    assert cups.cup_hash() == 20
+
+def test_cup_hash_10_rounds():
+    cups = Cups('389125467')
+    cups.play_game(10)
+    assert cups.cup_hash() == 18
+
+def test_cup_hash_100_rounds():
+    cups = Cups('389125467')
+    cups.play_game(100)
+    assert cups.cup_hash() == 42
+
+def test_big_sample():
+    cups = Cups('389125467', is_big=True)
+    cups.play_game(10000000)
+    assert cups.cup_hash() == '149245887792'
